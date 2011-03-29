d462a92704bfcee38ca07632904748f38da81c57
joschmid <joschmid>
2011-03-29 14:14:27 +0000

    <map server>: Added the methods used by the plugin
    git-svn-id: file:///var/pocketcampus/trunk@135 fd477b11-563a-408f-9d6e-eeb9ca009d0c

--- server/src/org/pocketcampus/plugin/map/Map.java
+++ server/src/org/pocketcampus/plugin/map/Map.java
@@ -18,9 +18,18 @@ public class Map extends MainRouter {
 	public String hello(HttpServletRequest request) {
 		return "Hello World";
 	}
+	
+	@PublicMethod
+	public String getLayers(HttpServletRequest request) {
+		return "{layers: [{id: 1, name: \"Restaurants\", description: \"List of restaurants\", icon: \"http://....\", cache: -1}, {id: 2, name: \"Rooms\", description: \"Rooms at EPFL\", icon: \"http://....\", cache: 60}, {id: 3, name: \"Friends\", description: \"Where your friends are\", icon: \"http://....\", cache: 0}]}";
+	}
+	
+	@PublicMethod
+	public String getItems(HttpServletRequest request) {
+		return "{layers: [{id: 1, items: [{name: \"Corbu\", description: \"Super\", longitude: \"...\", latitude: \"...\", altitude: \"...\"}, {name: \"Parmentier\", description: \"Numéro 318 s'il vous plait!\", longitude: \"...\", latitude: \"...\", altitude: \"...\"}]}, {id: 3, items: [{name: \"Jonas\", description: \"Youpi\", longitude: \"...\", latitude: \"...\", altitude: \"...\"}, {name: \"Johan\", description: \"What a good jerk dancer !\", longitude: \"...\", latitude: \"...\", altitude: \"...\"}]}]}";
+	}
 
 	protected String getDefaultMethod() {
 		return "map";
 	}
-
 }
