775afa2939e60d5a8e3b5b34134b40b3624d1dbf
Guillaume Ulrich <guillaume.ulrich@epfl.ch>
2011-03-31 14:30:01 +0000

    <URLLoader> addition of the getSource(String) method
    git-svn-id: file:///var/pocketcampus/trunk@156 fd477b11-563a-408f-9d6e-eeb9ca009d0c

--- shared/pocketcampus-shared/src/org/pocketcampus/shared/utils/URLLoader.java
+++ shared/pocketcampus-shared/src/org/pocketcampus/shared/utils/URLLoader.java
@@ -1,5 +1,29 @@
 package org.pocketcampus.shared.utils;
 
-public class URLLoader {
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
 
+public class URLLoader {
+	
+	public static String getSource(String url) throws IOException {
+		URL page = new URL(url);
+		InputStream is = page.openConnection().getInputStream();
+		
+		ArrayList<Byte> bytes = new ArrayList<Byte>(); 
+		int bte = is.read();
+		while(bte != -1) {
+			bytes.add((byte)bte);
+			bte = is.read();
+		}
+		
+		byte[] btes = new byte[bytes.size()];
+		for (int i = 0; i < btes.length; i++) {
+			btes[i] = bytes.get(i);
+		}
+		
+		return new String(btes);
+	}
+	
 }
