d6b7a8c493dc7c89c823752c7d7c4bd12fda0af2
Mohamed Tarek Benoudina <tarek.benoudina@epfl.ch>
2011-04-01 14:28:52 +0000

    Gps , wifi and Gsm Location as core service. hybrid as plugin "Classes ready , under test berfore commit"
    git-svn-id: file:///var/pocketcampus/trunk@174 fd477b11-563a-408f-9d6e-eeb9ca009d0c

---
+++ client/android/src/org/pocketcampus/core/service/positioning/GpsLocation.java
@@ -0,0 +1,5 @@
+package org.pocketcampus.core.service.positioning;
+
+public class GpsLocation {
+
+}
---
+++ client/android/src/org/pocketcampus/plugin/positioning/HybridLocation.java
@@ -0,0 +1,5 @@
+package org.pocketcampus.plugin.positioning;
+
+public class HybridLocation {
+
+}
