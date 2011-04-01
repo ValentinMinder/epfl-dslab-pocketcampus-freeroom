1d40333a466e25b0c2be5fd68c9e7d347f9ac0a4
Mohamed Tarek Benoudina <tarek.benoudina@epfl.ch>
2011-04-01 14:25:34 +0000

    Position class (core service) , reused from V1
    git-svn-id: file:///var/pocketcampus/trunk@173 fd477b11-563a-408f-9d6e-eeb9ca009d0c

---
+++ client/android/src/org/pocketcampus/core/service/positioning/Position.java
@@ -0,0 +1,60 @@
+/*
+ ********************* [ P O C K E T C A M P U S ] *****************
+ * [    LICENCE    ]     see "licence"-file in the root directory
+ * [   MAINTAINER  ]    tarek.benoudina@epfl.ch
+ * [     STATUS    ]    working
+ *
+ **************************[ C O M M E N T S ]**********************
+ *
+ *    Class reused from V1
+ *                      
+ *******************************************************************
+ */ 
+
+package org.pocketcampus.core.service.positioning;
+
+
+
+
+public class Position  {
+	
+	private final double lat_;
+	private final double lon_;
+	private final int level_;
+	
+	public Position(double lat, double lon, int level) {
+		this.lat_ = lat;
+		this.lon_ = lon;
+		this.level_ = level;
+	}
+	
+	public Position(double lat,double lon){
+		this.lat_ = lat;
+		this.lon_ = lon;
+		this.level_ = 0;
+	}
+	
+	public double getLat() {
+		return lat_;
+	}
+
+	public double getLon() {
+		return lon_;
+	}
+	
+	public int getLevel() {
+		return level_;
+	}
+	
+	@Override
+	public String toString() {
+		return "("+lat_+", "+lon_+", "+level_+")";
+	}
+	
+	@Override
+	public int hashCode() {
+		return toString().hashCode();
+	}
+	
+}
+
