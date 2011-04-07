409727d2ad67e3310a662e53437c8d59d2871473
Elodie Triponez <elodienilane.triponez@epfl.ch>
2011-04-07 18:18:35 +0000

    <food> parsing on server, not working yet
    git-svn-id: file:///var/pocketcampus/trunk@209 fd477b11-563a-408f-9d6e-eeb9ca009d0c

---
+++ server/src/org/pocketcampus/plugin/food/Food.java
@@ -0,0 +1,115 @@
+package org.pocketcampus.plugin.food;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.pocketcampus.core.router.IServerBase;
+import org.pocketcampus.core.router.PublicMethod;
+import org.pocketcampus.plugin.food.RssParser.RssFeed;
+import org.pocketcampus.shared.food.Meal;
+import org.pocketcampus.shared.food.Rating;
+import org.pocketcampus.shared.food.Restaurant;
+import org.pocketcampus.shared.food.StarRating;
+
+public class Food implements IServerBase {
+
+	private HashMap<Meal, Rating> campusMenu_;
+
+	/**
+	 * Parse the menus on startup.
+	 */
+	public Food() {
+		campusMenu_ = new HashMap<Meal, Rating>();
+		importMenus();
+		System.out.println("Importing menus");
+	}
+
+	@PublicMethod
+	public String food(HttpServletRequest request) {
+		return "Food tryouts.";
+	}
+
+	@PublicMethod
+	public HashMap<Meal, Rating> getMenus(HttpServletRequest request) {
+		return campusMenu_;
+	}
+
+	/**
+	 * Set rating for a particular meal
+	 * 
+	 * @param meal
+	 *            the meal for which we want to set the rating
+	 * @param rating
+	 *            the rating we want to put.
+	 * @return whether the operation worked.
+	 */
+//	@PublicMethod
+//	public boolean setRating(Meal meal, StarRating rating) {
+//		if (campusMenu_.containsKey(meal)) {
+//			//Average in the new rating with the ones previously there.
+//			Rating oldRating = campusMenu_.get(meal);
+//			double oldRatingValue = Restaurant.starRatingToDouble(oldRating
+//					.getValue());
+//			int oldRatingCount = oldRating.getNumberOfVotes();
+//			double newRatingValue = Restaurant.starRatingToDouble(rating);
+//
+//			Rating newMenuRating = new Rating(Restaurant
+//					.doubleToStarRating(((oldRatingValue) * oldRatingCount)
+//							+ newRatingValue), oldRatingCount + 1);
+//
+//			campusMenu_.put(meal, newMenuRating);
+//			return true;
+//		}
+//		return false;
+//	}
+
+	/**
+	 * Get the rating for a particular meal
+	 * 
+	 * @param meal
+	 *            the meal for which we want the rating
+	 * @return the corresponding rating.
+	 */
+//	@PublicMethod
+//	public Rating getRating(Meal meal) {
+//		return campusMenu_.get(meal);
+//	}
+
+	/**
+	 * Import menus from the Rss feeds
+	 */
+	private void importMenus() {
+		RestaurantListParser rlp = new RestaurantListParser();
+		HashMap<String, String> restaurantFeeds = rlp.getFeeds();
+		Set<String> restaurants = restaurantFeeds.keySet();
+
+		campusMenu_ = new HashMap<Meal, Rating>();
+
+		for (String r : restaurants) {
+			// For now, filter restaurants that cause encoding problems for
+			// current day.
+			if (!(r.equals("Parmentier") || r.equals("Le Vinci"))) {
+				System.out.println(r);
+				RssParser rp = new RssParser(restaurantFeeds.get(r));
+				rp.parse();
+				RssFeed feed = rp.getFeed();
+
+				Restaurant newResto = new Restaurant(r);
+				if (feed != null && feed.items != null) {
+					for (int i = 0; i < feed.items.size(); i++) {
+						Meal newMeal = new Meal(feed.items.get(i).title,
+								feed.items.get(i).description, newResto,
+								new Date(), true);
+						campusMenu_.put(newMeal, new Rating(
+								StarRating.STAR_3_0, 0));
+					}
+				} else {
+					System.out.println("Debug: feed null for " + r + ".");
+				}
+			}
+		}
+	}
+}
---
+++ server/src/org/pocketcampus/plugin/food/RestaurantListParser.java
@@ -0,0 +1,100 @@
+package org.pocketcampus.plugin.food;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+public class RestaurantListParser {
+
+	private HashMap<String, String> feeds;
+	private String feedString;
+
+	RestaurantListParser() {
+		feedString = getContents();
+		feeds = restaurantFeeds(feedString);
+	}
+
+	public HashMap<String, String> getFeeds() {
+		return feeds;
+	}
+
+	/**
+	 * Fetch the entire contents of a text file, and return it in a String. This
+	 * style of implementation does not throw Exceptions to the caller.
+	 * 
+	 * @param aFile
+	 *            is a file which already exists and can be read.
+	 */
+	private String getContents() {
+		// ...checks on aFile are elided
+		StringBuilder contents = new StringBuilder();
+
+		try {
+			// use buffering, reading one line at a time
+			InputStream instream = this.getClass().getResourceAsStream("restaurants_list.txt");
+
+			InputStreamReader inputreader = new InputStreamReader(instream);
+			BufferedReader input = new BufferedReader(inputreader);
+
+			try {
+				String line = null; // not declared within while loop
+				/*
+				 * readLine is a bit quirky : it returns the content of a line
+				 * MINUS the newline. it returns null only for the END of the
+				 * stream. it returns an empty String if two newlines appear in
+				 * a row.
+				 */
+				while ((line = input.readLine()) != null) {
+					contents.append(line);
+				}
+			} finally {
+				input.close();
+			}
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+
+		return contents.toString();
+	}
+
+	private HashMap<String, String> restaurantFeeds(String restaurantList) {
+		String tagRestaurant = "<Resto>";
+		String tagName = "<name>";
+		String tagUrl = "<rssfeed>";
+
+		String tagRestEnd = "</Resto>";
+		String tagNameEnd = "</name>";
+		String tagUrlEnd = "</rssfeed>";
+
+		HashMap<String, String> feeds = new HashMap<String, String>();
+
+		while (restaurantList.length() > 1) {
+			// Restaurant
+			int startResto = restaurantList.indexOf(tagRestaurant);
+			int endResto = restaurantList.indexOf(tagRestEnd);
+			String restAttributes = restaurantList.substring(
+					startResto + tagRestaurant.length(), endResto).trim();
+
+			// Restaurant Names
+			int startName = restAttributes.indexOf(tagName);
+			int endName = restAttributes.indexOf(tagNameEnd);
+			String restaurantName = restAttributes.substring(
+					startName + tagName.length(), endName).trim();
+
+			// Restaurant Feeds
+			int startUrl = restAttributes.indexOf(tagUrl);
+			int endUrl = restAttributes.indexOf(tagUrlEnd);
+			String restaurantUrl = restAttributes.substring(
+					startUrl + tagUrl.length(), endUrl).trim();
+
+			feeds.put(restaurantName, restaurantUrl);
+			restaurantList = restaurantList.substring(
+					endResto + tagRestEnd.length(), restaurantList.length())
+					.trim();
+		}
+
+		return feeds;
+	}
+}
