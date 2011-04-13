5da9885702c947c29145be98d2ff143582ce2e46
Mohamed Tarek Benoudina <tarek.benoudina@epfl.ch>
2011-04-13 14:37:38 +0000

    directory changed
    git-svn-id: file:///var/pocketcampus/trunk@267 fd477b11-563a-408f-9d6e-eeb9ca009d0c

---
+++ client/android/src/org/pocketcampus/provider/positioning/Position.java
@@ -0,0 +1,67 @@
+package org.pocketcampus.provider.positioning;
+
+import java.io.Serializable;
+
+public class Position implements Serializable {
+	private static final long serialVersionUID = -8760312491181393320L;
+	private double latitude_;
+	private double longitude_;
+	private double altitude_;
+	private int level_;
+	
+	public Position(){}
+	
+	public Position(double lat, double lon, double alt) {
+		this.latitude_ = lat;
+		this.longitude_ = lon;
+		this.altitude_ = alt;
+	}
+	
+	public Position(int lat, int lon, int alt) {
+		this.latitude_ = lat / 1E6;
+		this.longitude_ = lon / 1E6;
+		this.altitude_ = alt;
+	}
+	
+	public Position(double lat,double lon , int lev){
+		this.latitude_=lat;
+		this.longitude_=lon;
+		this.level_=lev;
+	}
+	
+	public double getLatitude() {
+		return latitude_;
+	}
+	
+	public int getLatitudeE6() {
+		return (int) (latitude_ * 1E6);
+	}
+
+	public double getLongitude() {
+		return longitude_;
+	}
+	
+	public int getLongitudeE6() {
+		return (int) (longitude_ * 1E6);
+	}
+	
+	public double getAltitude() {
+		return altitude_;
+	}
+	
+	@Override
+	public String toString() {
+		return "("+latitude_+", "+longitude_+", "+altitude_+")";
+	}
+	
+	@Override
+	public int hashCode() {
+		return toString().hashCode();
+	}
+
+	public String getLevel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
+
