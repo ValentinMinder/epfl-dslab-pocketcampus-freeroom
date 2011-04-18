f6d4c79bab5d67f57b927e07da9714efd9188bbe
joleuenb <joleuenb>
2011-04-18 16:54:35 +0000

    <server> requesthandler: url of amazon server added
    git-svn-id: file:///var/pocketcampus/trunk@328 fd477b11-563a-408f-9d6e-eeb9ca009d0c

--- client/android/src/org/pocketcampus/core/communication/RequestHandler.java
+++ client/android/src/org/pocketcampus/core/communication/RequestHandler.java
@@ -19,11 +19,12 @@ public class RequestHandler {
 	//private String serverUrl_ = "http://128.178.240.75:8080/pocketcampus-server/";
 	
 	// JOHAN LOCAL SERVER URL
-	private String serverUrl_ = "http://192.168.1.46:8080/pocketcampus-server/";
+	//private String serverUrl_ = "http://192.168.1.46:8080/pocketcampus-server/";
 	//private String serverUrl_ = "http://128.178.242.103:8080/pocketcampus-server/";
 
 	// JOHAN DEBUG STATIC SERVER
 	//private String serverUrl_ = "http://jleuleu.neqo.org/temp/";
+	private String serverUrl_ = "http://ec2-46-51-131-245.eu-west-1.compute.amazonaws.com/pocketcampus-server/";
 
 
 	public RequestHandler(PluginInfo pluginInfo) {
