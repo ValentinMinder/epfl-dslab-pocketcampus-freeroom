56cbbdcc5406116bcd0a473f7e7d38ab7ec48ebe
Guillaume Ulrich <guillaume.ulrich@epfl.ch>
2011-04-23 08:53:44 +0000

    <Mainscreen> - Forgot to add the id field in the MainscreenNews class
    git-svn-id: file:///var/pocketcampus/trunk@373 fd477b11-563a-408f-9d6e-eeb9ca009d0c

--- client/android/src/org/pocketcampus/plugin/mainscreen/MainscreenNews.java
+++ client/android/src/org/pocketcampus/plugin/mainscreen/MainscreenNews.java
@@ -1,13 +1,24 @@
 package org.pocketcampus.plugin.mainscreen;
 
+/**
+ * 
+ * This class contains the information needed to display a news on the mainscreen
+ * 
+ * @status complete
+ * 
+ * @author Guillaume
+ *
+ */
 public class MainscreenNews {
 
 	private String title_;
 	private String content_;
+	private int id_;
 	
-	public MainscreenNews(String title, String content) {
+	public MainscreenNews(String title, String content, int id) {
 		this.content_ = content;
 		this.title_ = title;
+		this.id_ = id;
 	}
 	
 	public String getContent_() {
@@ -16,6 +27,10 @@ public class MainscreenNews {
 	
 	public String getTitle_() {
 		return title_;
+	}
+	
+	public int getId_() {
+		return id_;
 	}
 	
 }
