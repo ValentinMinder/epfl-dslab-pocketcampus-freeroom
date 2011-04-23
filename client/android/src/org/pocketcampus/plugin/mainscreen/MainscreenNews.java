fc765284c9a0c5052748c6f655a4a6f65df0d195
Guillaume Ulrich <guillaume.ulrich@epfl.ch>
2011-04-23 08:55:33 +0000

    <Mainscreen> - Added a PluginBase to the MainscreenNews class, in order to launch the plugin and get the icon
    git-svn-id: file:///var/pocketcampus/trunk@374 fd477b11-563a-408f-9d6e-eeb9ca009d0c

--- client/android/src/org/pocketcampus/plugin/mainscreen/MainscreenNews.java
+++ client/android/src/org/pocketcampus/plugin/mainscreen/MainscreenNews.java
@@ -1,5 +1,7 @@
 package org.pocketcampus.plugin.mainscreen;
 
+import org.pocketcampus.core.plugin.PluginBase;
+
 /**
  * 
  * This class contains the information needed to display a news on the mainscreen
@@ -14,11 +16,13 @@ public class MainscreenNews {
 	private String title_;
 	private String content_;
 	private int id_;
+	private PluginBase plugin_;
 	
-	public MainscreenNews(String title, String content, int id) {
+	public MainscreenNews(String title, String content, int id, PluginBase plugin) {
 		this.content_ = content;
 		this.title_ = title;
 		this.id_ = id;
+		this.plugin_ = plugin;
 	}
 	
 	public String getContent_() {
@@ -31,6 +35,10 @@ public class MainscreenNews {
 	
 	public int getId_() {
 		return id_;
+	}
+	
+	public PluginBase getPlugin_() {
+		return plugin_;
 	}
 	
 }
