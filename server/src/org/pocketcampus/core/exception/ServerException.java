0e54c101cb44b55e42e022a8d09d0003cde2a471
menetrey <menetrey>
2011-05-02 10:41:53 +0000

    <core> Updated ServerException to follow the standard exception chaining model. All previous constructors and method are kept (compatibility : 100%), new ones added.
    git-svn-id: file:///var/pocketcampus/trunk@417 fd477b11-563a-408f-9d6e-eeb9ca009d0c

--- server/src/org/pocketcampus/core/exception/ServerException.java
+++ server/src/org/pocketcampus/core/exception/ServerException.java
@@ -2,18 +2,30 @@ package org.pocketcampus.core.exception;
 
 public class ServerException extends Exception {
 
-	private static final long serialVersionUID = -7108977658617157470L;
-	private String explanation_;
-
-	public ServerException(String explanation) {
-		explanation_ = explanation;
-	}
+	private static final long serialVersionUID = -9190624100724525654L;
+	private static final String defaultMessage_ = "No explanation available";
 	
 	public ServerException() {
-		explanation_ = "No explanation available.";
+		super(defaultMessage_);
 	}
-
+	
+	public ServerException(String explanation) {
+		super(explanation);
+	}
+	
+	public ServerException(Throwable t) {
+		super(defaultMessage_, t);
+	}
+	
+	public ServerException(String explanation, Throwable t) {
+		super(explanation, t);
+	}
+	
+	/**
+	 * Returns the explanation of this ServerError.
+	 * @return the explanation of this ServerError instance
+	 */
 	public String getExplanation() {
-		return explanation_;
+		return super.getMessage();
 	}
 }
