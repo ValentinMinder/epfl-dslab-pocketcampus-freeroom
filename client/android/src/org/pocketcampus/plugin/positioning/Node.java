2ba0825a763a296388e417dd9d27d399b5a89eff
Mohamed Tarek Benoudina <tarek.benoudina@epfl.ch>
2011-05-04 21:33:14 +0000

    new class Node , represents the referencial points in the grid
    git-svn-id: file:///var/pocketcampus/trunk@446 fd477b11-563a-408f-9d6e-eeb9ca009d0c

---
+++ client/android/src/org/pocketcampus/plugin/positioning/Node.java
@@ -0,0 +1,33 @@
+package org.pocketcampus.plugin.positioning;
+
+public class Node {
+	
+	private CartesianPoint coordinates_;
+	private int value_;
+	
+	public Node(CartesianPoint _coord, int _value){
+		this.coordinates_ = _coord;
+		this.value_ = _value;
+	}
+
+	public CartesianPoint getCoordinates() {
+		return coordinates_;
+	}
+
+	public void setCoordinates(CartesianPoint coordinates_) {
+		this.coordinates_ = coordinates_;
+	}
+
+	public int getValue() {
+		return value_;
+	}
+
+	public void setValue(int value_) {
+		this.value_ = value_;
+	}
+	
+	
+	
+	
+
+}
