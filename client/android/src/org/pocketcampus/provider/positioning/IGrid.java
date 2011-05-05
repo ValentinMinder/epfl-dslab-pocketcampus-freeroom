ce6f34384fe324b765dcf73343aeff6fb112020d
Mohamed Tarek Benoudina <tarek.benoudina@epfl.ch>
2011-05-05 07:59:19 +0000

    method added
    git-svn-id: file:///var/pocketcampus/trunk@462 fd477b11-563a-408f-9d6e-eeb9ca009d0c

--- client/android/src/org/pocketcampus/provider/positioning/IGrid.java
+++ client/android/src/org/pocketcampus/provider/positioning/IGrid.java
@@ -1,5 +1,7 @@
 package org.pocketcampus.provider.positioning;
 
+import java.util.List;
+
 import org.pocketcampus.plugin.positioning.AccessPoint;
 import org.pocketcampus.plugin.positioning.CartesianPoint;
 import org.pocketcampus.shared.plugin.map.Position;
@@ -7,6 +9,7 @@ import org.pocketcampus.shared.plugin.map.Position;
 public interface IGrid {
 
 public Position getInitialPosition();
+public List<AccessPoint> getApGrid();
 public double getDMax();
 public double getDMin();
 public int getNodeMin();
