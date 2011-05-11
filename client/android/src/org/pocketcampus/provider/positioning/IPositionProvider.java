0fc103c040932b4f03747c78edfe97ab79cc4b35
Mohamed Tarek Benoudina <tarek.benoudina@epfl.ch>
2011-05-11 12:53:17 +0000

    <Positioning> Provider rename
    git-svn-id: file:///var/pocketcampus/trunk@619 fd477b11-563a-408f-9d6e-eeb9ca009d0c

---
+++ client/android/src/org/pocketcampus/provider/positioning/IPositionProvider.java
@@ -0,0 +1,18 @@
+package org.pocketcampus.provider.positioning;
+
+import java.io.IOException;
+
+import org.osmdroid.util.GeoPoint;
+import org.pocketcampus.shared.plugin.map.Position;
+
+
+public interface IPositionProvider {
+
+	
+public Position getPosition();
+public GeoPoint getGsmPosition() throws IOException;
+public Position getGpsPosition();
+//public Position getHybridLocation();
+public double getAccuracy();
+	
+}
