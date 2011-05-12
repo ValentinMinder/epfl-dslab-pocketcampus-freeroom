fb1a1911d8cd90e526bb28202796ed3435e556a9
Mohamed Tarek Benoudina <tarek.benoudina@epfl.ch>
2011-05-12 08:47:38 +0000

    <Positioning>
    git-svn-id: file:///var/pocketcampus/trunk@636 fd477b11-563a-408f-9d6e-eeb9ca009d0c

--- client/android/src/org/pocketcampus/plugin/positioning/Accelerometer.java
+++ client/android/src/org/pocketcampus/plugin/positioning/Accelerometer.java
@@ -1,6 +1,27 @@
 package org.pocketcampus.plugin.positioning;
 
+import android.app.Service;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+
 public class Accelerometer {
+	
+	private SensorManager sensorManager_;
+	private final Sensor accelerometer_;
+	private Context context_;
+	private String SENSOR_SERVICE;
+	private double xValue;
+	private double yValue;
+	private double zValue;
+	
+	
+	public Accelerometer(Context context){
+		context_ = context;
+		sensorManager_ = (SensorManager)context.getSystemService(SENSOR_SERVICE);
+        accelerometer_ = sensorManager_.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+	}
 
 	public boolean onShake() {
 		// TODO Auto-generated method stub
