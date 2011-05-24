5c651fa1d550d82e68da3c0f052c7f32fafad55e
joleuenb <joleuenb>
2011-05-24 20:06:32 +0000

    <map/position> method to check if a location is inside the campus area
    git-svn-id: file:///var/pocketcampus/trunk@813 fd477b11-563a-408f-9d6e-eeb9ca009d0c

---
+++ client/android/src/org/pocketcampus/utils/PositionUtil.java
@@ -0,0 +1,27 @@
+package org.pocketcampus.utils;
+
+import org.pocketcampus.R;
+
+import android.content.Context;
+import android.location.Location;
+
+public class PositionUtil {
+	
+	/**
+	 * Checks if the given location is inside the campus area
+	 * @param context the application context (needed to obtain the resources)
+	 * @param location the location to check.
+	 * @return true if the location is inside the campus area, false otherwise.
+	 */
+	public static boolean isLocationOnCampus(Context context, Location location) {
+		double minLat = Double.parseDouble(context.getResources().getString(R.string.map_campus_min_latitude));
+		double maxLat = Double.parseDouble(context.getResources().getString(R.string.map_campus_max_latitude));
+		double minLon = Double.parseDouble(context.getResources().getString(R.string.map_campus_min_longitude));
+		double maxLon = Double.parseDouble(context.getResources().getString(R.string.map_campus_max_longitude));
+		
+		return (location.getLatitude() > minLat &&
+				location.getLatitude() < maxLat &&
+				location.getLongitude() > minLon &&
+				location.getLatitude() < maxLon);
+	}
+}
