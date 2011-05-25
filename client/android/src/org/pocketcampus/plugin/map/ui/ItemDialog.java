05e72361187b38dca3bf5afdedb632ba15e46594
Oriane Rodriguez <oriane.rodriguez@epfl.ch>
2011-05-25 12:29:42 +0000

    <food/map> Filtered to open FoodPlugin only when you click on a Cafeteria/Restaurant that actually have menus (not INM for example)
    git-svn-id: file:///var/pocketcampus/trunk@820 fd477b11-563a-408f-9d6e-eeb9ca009d0c

--- client/android/src/org/pocketcampus/plugin/map/ui/ItemDialog.java
+++ client/android/src/org/pocketcampus/plugin/map/ui/ItemDialog.java
@@ -10,6 +10,8 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
+import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Show a dialog box when the user clicks on an item on the map
@@ -20,7 +22,7 @@ import android.net.Uri;
  *
  */
 public class ItemDialog {
-	
+
 	private MapPlugin mp_;
 	private MapElement item_;
 
@@ -52,6 +54,8 @@ public class ItemDialog {
 
 		// The item has a plugin linked to it
 		final String pluginId = item_.getPluginId();
+		Log.d("FoodPlugin", "Plugin ID : " + pluginId);
+
 		if(pluginId != null && !"".equals(pluginId)) {
 			builder.setNegativeButton(mp_.getResources().getString(R.string.map_open_plugin_button), new DialogInterface.OnClickListener() {
 				public void onClick(DialogInterface dialog, int id) {
@@ -59,7 +63,15 @@ public class ItemDialog {
 						Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(pluginId.substring(MapPlugin.ITEM_GO_URL.length())));
 						mp_.startActivity(intent);
 					} else {
-						Core.startPluginWithID(mp_, pluginId, item_.getItemId());
+						if(pluginId.equals("org.pocketcampus.plugin.food.FoodPlugin")){
+							if(isRestaurant(item_.getItemId())){
+								Core.startPluginWithID(mp_, pluginId, item_.getItemId());
+							}else{
+								Toast.makeText(mp_, mp_.getResources().getString(R.string.food_no_additional_information), Toast.LENGTH_LONG).show();
+							}
+						}else{
+							Core.startPluginWithID(mp_, pluginId, item_.getItemId());
+						}
 					}
 					dialog.dismiss();	
 				}
@@ -71,6 +83,31 @@ public class ItemDialog {
 		alert.setCanceledOnTouchOutside(true);
 
 		alert.show();
+	}
+	
+	private boolean isRestaurant(int id){
+		boolean isRestaurant = false;
+		
+		switch (id){
+		case 39321 :
+		case 39322 :
+		case 39323 :
+		case 39324 :
+		case 39326 :
+		case 39327 :
+		case 39330 :
+		case 39333 :
+		case 39335 :
+		case 39337 :
+		case 39338 :
+			isRestaurant = true;
+			break;
+		default :
+			isRestaurant = false;
+			break;
+		}
+		
+		return isRestaurant;
 	}
 
 }
