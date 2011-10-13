4b426b158f3250e6b09a2149094702f63a1ca5e0
Florian Laurent <florian.laurent@epfl.ch>
2011-10-13 20:51:28 +0000

    git-svn-id: file:///var/pocketcampus/trunk@978 fd477b11-563a-408f-9d6e-eeb9ca009d0c

---
+++ tool/tool.thriftcompiler/src/org/pocketcampus/tool/thriftcompiler/LocalConfig.java
@@ -0,0 +1,19 @@
+package org.pocketcampus.tool.thriftcompiler;
+
+/**
+ * PLEASE DON'T COMMIT THIS FILE!
+ * 
+ * Local settings for the Thrift compiler.
+ *  
+ * @author Florian
+ */
+public class LocalConfig {
+	// IF THIS SCRIPT ISN'T WORKING
+	// you probably need to fix Thrift path below.
+	
+	// if you're on Linux
+	//private static final String THRIFT_PATH = "/usr/local/bin/thrift";
+	
+	// if you're on Windows
+	public static final String THRIFT_PATH = "C:/thrift/thrift-0.7.0.exe";
+}
