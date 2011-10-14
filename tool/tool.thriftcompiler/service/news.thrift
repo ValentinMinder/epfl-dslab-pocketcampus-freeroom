1258ebe193d0b79579e917c8e614b3c48e616e0c
Elodie Triponez <elodienilane.triponez@epfl.ch>
2011-10-14 10:18:07 +0000

    <news> changed thrift argument number
    git-svn-id: file:///var/pocketcampus/trunk@987 fd477b11-563a-408f-9d6e-eeb9ca009d0c

--- tool/tool.thriftcompiler/service/news.thrift
+++ tool/tool.thriftcompiler/service/news.thrift
@@ -1,3 +1,28 @@
 namespace java org.pocketcampus.plugin.news.shared
 
 include "../include/common.thrift"
+
+typedef i32 int
+typedef i64 timestamp
+
+struct NewsItem {
+	1: required common.Id id;
+	2: required string title;
+	3: required string description;
+	4: required string link;
+	5: required string pubDate;
+	6: required timestamp pubDateDate;
+	7: required string imageUrl;
+}
+
+struct Feed {
+	1: required common.Id id;
+	2: required string title;
+	3: required string link;
+	4: required string description;
+	5: required list<NewsItem> items;
+}
+
+service NewsService {
+	list<NewsItem> getNewsItems(1: list<string> feedUrls);
+}
