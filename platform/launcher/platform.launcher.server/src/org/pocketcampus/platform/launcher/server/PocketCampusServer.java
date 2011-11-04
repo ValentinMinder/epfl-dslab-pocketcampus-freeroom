da3c02b52658846d3140c6c2e8782d8a25542ff2
scheiben <scheiben>
2011-11-04 10:16:20 +0000

    git-svn-id: file:///var/pocketcampus/trunk@1114 fd477b11-563a-408f-9d6e-eeb9ca009d0c

--- platform/launcher/platform.launcher.server/src/org/pocketcampus/platform/launcher/server/PocketCampusServer.java
+++ platform/launcher/platform.launcher.server/src/org/pocketcampus/platform/launcher/server/PocketCampusServer.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 
 import org.pocketcampus.authentication.server.AuthenticationServiceImpl;
 import org.pocketcampus.plugin.authentication.shared.AuthenticationService;
+import org.pocketcampus.plugin.bikes.server.BikesServiceImpl;
+import org.pocketcampus.plugin.bikes.shared.BikeService;
 import org.pocketcampus.plugin.camipro.server.CamiproServiceImpl;
 import org.pocketcampus.plugin.camipro.shared.CamiproService;
 import org.pocketcampus.plugin.directory.server.DirectoryServiceImpl;
@@ -21,7 +23,6 @@ import org.pocketcampus.plugin.takeoutreceiver.shared.TakeoutReceiverService;
 import org.pocketcampus.plugin.test.shared.TestService;
 import org.pocketcampus.server.plugin.takeout.TakeoutGatewayImpl;
 import org.pocketcampus.server.plugin.takeout.TakeoutOrderServiceImpl;
-import org.pocketcampus.server.plugin.takeoutreceiver.TakeoutReceiverServiceImpl;
 
 public class PocketCampusServer extends ServerBase {
 
@@ -41,14 +42,17 @@ public class PocketCampusServer extends ServerBase {
 		// The name must match the plugin name given in the plugin controller (and must be unique!)
 		// TODO create a unique id directly in the thrift file when compiling the thrift files?
 		processors.add(new Processor(new TestService.Processor<TestServiceImpl>(new TestServiceImpl()), "test"));
-		processors.add(new Processor(new TakeoutReceiverService.Processor<TakeoutReceiverServiceImpl>(new TakeoutReceiverServiceImpl()), "takeoutreceiver"));
-		processors.add(new Processor(new FoodService.Processor<FoodServiceImpl>(new FoodServiceImpl()), "food"));
-		processors.add(new Processor(new TakeoutGateway.Processor<TakeoutGatewayImpl>(new TakeoutGatewayImpl()), "takeout-gateway"));
-		processors.add(new Processor(new DirectoryService.Processor<DirectoryServiceImpl>(new DirectoryServiceImpl()), "directory"));
-		processors.add(new Processor(new TakeoutOrderService.Processor<TakeoutOrderServiceImpl>(new TakeoutOrderServiceImpl()), "takeout-order"));
+//		processors.add(new Processor(new TakeoutReceiverService.Processor<TakeoutReceiverServiceImpl>(new TakeoutReceiverServiceImpl()), "takeoutreceiver"));
+//		processors.add(new Processor(new FoodService.Processor<FoodServiceImpl>(new FoodServiceImpl()), "food"));
+//		processors.add(new Processor(new TakeoutGateway.Processor<TakeoutGatewayImpl>(new TakeoutGatewayImpl()), "takeout-gateway"));
+//		processors.add(new Processor(new DirectoryService.Processor<DirectoryServiceImpl>(new DirectoryServiceImpl()), "directory"));
+//		processors.add(new Processor(new FoodService.Processor<FoodServiceImpl>(new FoodServiceImpl()), "food"));
+//		processors.add(new Processor(new TakeoutGateway.Processor<TakeoutGatewayImpl>(new TakeoutGatewayImpl()), "takeout-gateway"));
+		processors.add(new Processor(new BikeService.Processor<BikesServiceImpl>(new BikesServiceImpl()), "bikes"));
+//		processors.add(new Processor(new TakeoutOrderService.Processor<TakeoutOrderServiceImpl>(new TakeoutOrderServiceImpl()), "takeout-order"));
 		processors.add(new Processor(new AuthenticationService.Processor<AuthenticationServiceImpl>(new AuthenticationServiceImpl()), "authentication"));
-		processors.add(new Processor(new CamiproService.Processor<CamiproServiceImpl>(new CamiproServiceImpl()), "camipro"));
-		processors.add(new Processor(new NewsService.Processor<NewsServiceImpl>(new NewsServiceImpl()), "news"));
+//		processors.add(new Processor(new CamiproService.Processor<CamiproServiceImpl>(new CamiproServiceImpl()), "camipro"));
+
 
 		
 		return processors;
