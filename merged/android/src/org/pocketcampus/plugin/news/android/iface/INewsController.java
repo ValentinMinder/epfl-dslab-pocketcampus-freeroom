de801bf7a30014d0d5f278cd74538861285355a5
Oriane Rodriguez <oriane.rodriguez@epfl.ch>
2011-11-16 19:58:36 +0000

    <App> Trying again!
    git-svn-id: file:///var/pocketcampus/trunk@1238 fd477b11-563a-408f-9d6e-eeb9ca009d0c

--- merged/android/src/org/pocketcampus/plugin/news/android/iface/INewsController.java
+++
@@ -1,7 +0,0 @@
-package org.pocketcampus.plugin.news.android.iface;
-
-public interface INewsController {
-
-	void loadNews();
-
-}
--- merged/android/src/org/pocketcampus/plugin/news/android/iface/INewsView.java
+++
@@ -1,9 +0,0 @@
-package org.pocketcampus.plugin.news.android.iface;
-
-import org.pocketcampus.android.platform.sdk.core.IView;
-
-public interface INewsView extends IView {
-
-	void newsUpdated();
-
-}
