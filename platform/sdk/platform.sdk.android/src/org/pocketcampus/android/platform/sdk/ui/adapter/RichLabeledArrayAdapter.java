40505eb8100d174f2f448e9c3a57a5c14d821f6d
Oriane Rodriguez <oriane.rodriguez@epfl.ch>
2011-11-20 00:18:37 +0000

    <sdk> Added a RichLabeledListViewElement along woth its Adapter and View, to display elements with a title, a description, some value like a price, and a date.
    git-svn-id: file:///var/pocketcampus/trunk@1268 fd477b11-563a-408f-9d6e-eeb9ca009d0c

---
+++ platform/sdk/platform.sdk.android/src/org/pocketcampus/android/platform/sdk/ui/adapter/RichLabeledArrayAdapter.java
@@ -0,0 +1,57 @@
+package org.pocketcampus.android.platform.sdk.ui.adapter;
+
+import java.util.List;
+
+import org.pocketcampus.android.platform.sdk.ui.element.RichView;
+import org.pocketcampus.android.platform.sdk.ui.labeler.IRichLabeler;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView.OnItemClickListener;
+
+/**
+ * An Adapter to handle the display of rich labeled Objects
+ * 
+ * @author Oriane <oriane.rodriguez@epfl.ch>
+ */
+public class RichLabeledArrayAdapter extends AbstractArrayAdapter {
+	/** The RichLabeler to get the object's attributes */
+	private IRichLabeler mLabeler;
+	/** The application context */
+	private Context mContext;
+	/** The listener on the object line */
+	private OnItemClickListener mOnClickListener;
+
+	/**
+	 * The constructor
+	 * 
+	 * @param context
+	 *            The application context
+	 * @param items
+	 *            The items to display
+	 * @param labeler
+	 *            The RichLabeler to get the object's attributes
+	 */
+	public RichLabeledArrayAdapter(Context context,
+			List<? extends Object> items, IRichLabeler<? extends Object> labeler) {
+		super(context, items);
+
+		mContext = context;
+		mLabeler = labeler;
+	}
+
+	/**
+	 * Overrides the getView method and creates an RichView for each labeled
+	 * object
+	 */
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View v = new RichView(getItem(position), mContext, mLabeler, mOnClickListener, position);
+		return v;
+	}
+	
+	public void setOnLineClickLIstener(OnItemClickListener onLineListener) {
+		mOnClickListener = onLineListener;
+	}
+}
---
+++ platform/sdk/platform.sdk.android/src/org/pocketcampus/android/platform/sdk/ui/element/RichView.java
@@ -0,0 +1,161 @@
+package org.pocketcampus.android.platform.sdk.ui.element;
+
+import java.util.Date;
+
+import org.pocketcampus.R;
+import org.pocketcampus.android.platform.sdk.ui.labeler.IRichLabeler;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * A view to display an Object that the user can rate. It represents a line of a
+ * ListView and contains the object's title and description, along with a rating
+ * bar and the number of votes that Object got. It's designed to be used with
+ * the RatableExpandableListView or an equivalent, and can be created directly
+ * in the Application View.
+ * 
+ * @author Oriane <oriane.rodriguez@epfl.ch>
+ * @author Elodie <elodienilane.triponez@epfl.ch>
+ */
+public class RichView extends LinearLayout {
+	/** The Application Context */
+	private Context mContext;
+	/** The convert view */
+	private View mConvertView;
+	/** The layout */
+	private LinearLayout mLayout;
+
+	/** The Object to be displayed in the View */
+	private Object mCurrentObject;
+	/** The Labeler from the Application to get the Obejct's attributes */
+	private IRichLabeler mLabeler;
+	/** The position of the Object in the ListView */
+	private int mPosition;
+	/** The Object's title */
+	private TextView mTitleLine;
+	/** The Object's description */
+	private TextView mDescriptionLine;
+	/** The Object's number of votes */
+	private TextView mValueLine;
+	/** The Object's date */
+	private TextView mDateLine;
+
+	/** The click listener on the Object's line */
+	private OnItemClickListener mOnElementClickLIstener;
+
+	/**
+	 * The constructor
+	 * 
+	 * @param currentObject
+	 *            The Object to be displayed in the line
+	 * @param context
+	 *            The Application context
+	 * @param labeler
+	 *            The Object's labeler
+	 * @param elementListener
+	 *            the listener for the title and description lines
+	 * @param ratingListener
+	 *            the listener on the rating bar
+	 * @param position
+	 *            the position of the Object in the List
+	 */
+	public RichView(Object currentObject, Context context,
+			IRichLabeler<? extends Object> labeler,
+			OnItemClickListener elementListener, int position) {
+		super(context);
+		mContext = context;
+		mConvertView = LayoutInflater.from(context.getApplicationContext())
+				.inflate(R.layout.sdk_list_entry_rich_view, null);
+
+		mCurrentObject = currentObject;
+		mLabeler = labeler;
+		mPosition = position;
+
+		/**
+		 * Creates a ViewHolder and store references to the two children views
+		 * we want to bind data to.
+		 */
+		mLayout = (LinearLayout) mConvertView
+				.findViewById(R.id.sdk_list_entry_rich_view_layout);
+		mTitleLine = (TextView) mConvertView
+				.findViewById(R.id.sdk_list_entry_rich_view_title);
+		mDescriptionLine = (TextView) mConvertView
+				.findViewById(R.id.sdk_list_entry_rich_view_description);
+		mValueLine = (TextView) mConvertView
+				.findViewById(R.id.sdk_list_entry_rich_view_value);
+		mDateLine = (TextView) mConvertView
+				.findViewById(R.id.sdk_list_entry_rich_view_date);
+
+		/** Listener */
+		mOnElementClickLIstener = elementListener;
+
+		initializeView();
+	}
+
+	/**
+	 * Initializes the View
+	 */
+	public void initializeView() {
+
+		/** Sets the click listener on the layout (on the line) */
+		mLayout.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				if (mOnElementClickLIstener != null) {
+					v.setTag(mLabeler.getTitle(mCurrentObject));
+					mOnElementClickLIstener.onItemClick(null, v, mPosition, 0);
+				}
+			}
+		});
+
+		/** Bind the data efficiently with the holder. */
+
+		/** Title */
+		mTitleLine.setText(mLabeler.getTitle(mCurrentObject));
+		/** Description */
+		mDescriptionLine.setText(mLabeler.getDescription(mCurrentObject));
+		/** Price */
+		mValueLine.setText("" + mLabeler.getValue(mCurrentObject));
+		/** Date */
+		Date date = mLabeler.getDate(mCurrentObject);
+		mDateLine.setText("" + day(date) + " " + (date.getMonth()+1) + ", "
+				+ date.getHours() + ":" + date.getMinutes());
+
+		addView(mConvertView);
+	}
+
+	private String day(Date date){
+		String day = "";
+		
+		switch (date.getDay()) {
+		case 0 :
+			day = mContext.getResources().getString(R.string.sdk_day_sunday);
+			break;
+		case 1 :
+			day = mContext.getResources().getString(R.string.sdk_day_monday);
+			break;
+		case 2 :
+			day = mContext.getResources().getString(R.string.sdk_day_tuesday);
+			break;
+		case 3 :
+			day = mContext.getResources().getString(R.string.sdk_day_wednesday);
+			break;
+		case 4 :
+			day = mContext.getResources().getString(R.string.sdk_day_thursday);
+			break;
+		case 5 :
+			day = mContext.getResources().getString(R.string.sdk_day_friday);
+			break;
+		case 6 :
+			day = mContext.getResources().getString(R.string.sdk_day_saturday);
+		default :
+			break;
+		}
+		
+		return day;
+	}
+ }
