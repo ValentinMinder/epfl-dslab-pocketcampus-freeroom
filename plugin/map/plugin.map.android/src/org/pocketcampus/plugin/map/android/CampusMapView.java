767b5e1e5df37bb9662912183497e9625638dea9
Florian Laurent <florian.laurent@epfl.ch>
2011-11-28 18:33:33 +0000

    <map> first map commit
    git-svn-id: file:///var/pocketcampus/trunk@1340 fd477b11-563a-408f-9d6e-eeb9ca009d0c

---
+++ plugin/map/plugin.map.android/src/org/pocketcampus/plugin/map/android/CampusMapView.java
@@ -0,0 +1,14 @@
+package org.pocketcampus.plugin.map.android;
+
+import org.osmdroid.views.MapView;
+
+import android.content.Context;
+
+public class CampusMapView extends MapView {
+
+	public CampusMapView(Context context, int tileSizePixels) {
+		super(context, tileSizePixels);
+	}
+	
+	
+}
