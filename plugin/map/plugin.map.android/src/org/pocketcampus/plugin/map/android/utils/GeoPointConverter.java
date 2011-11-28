d9acfda547e85fdd166cf0e5ae54f975ec2a4b2f
Florian Laurent <florian.laurent@epfl.ch>
2011-11-28 20:01:00 +0000

    Utils files
    git-svn-id: file:///var/pocketcampus/trunk@1344 fd477b11-563a-408f-9d6e-eeb9ca009d0c

---
+++ plugin/map/plugin.map.android/src/org/pocketcampus/plugin/map/android/utils/GeoPointConverter.java
@@ -0,0 +1,10 @@
+package org.pocketcampus.plugin.map.android.utils;
+
+import org.osmdroid.util.GeoPoint;
+import org.pocketcampus.plugin.map.android.shared.Position;
+
+public class GeoPointConverter {
+	public static Position toPosition(GeoPoint g) {
+		return new Position(g.getLatitudeE6(), g.getLongitudeE6(), g.getAltitude());
+	}
+}
