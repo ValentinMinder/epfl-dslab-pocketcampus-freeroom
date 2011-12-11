34829bd3a55390ddda642b771c6db6898f04d8c3
Florian Laurent <florian.laurent@epfl.ch>
2011-12-11 19:20:40 +0000

    <takeout> updating thrift definition
    git-svn-id: file:///var/pocketcampus/trunk@1437 fd477b11-563a-408f-9d6e-eeb9ca009d0c

--- tool/tool.thriftcompiler/service/takeout.thrift
+++ tool/tool.thriftcompiler/service/takeout.thrift
@@ -4,7 +4,7 @@ include "../include/common.thrift"
 include "../include/restaurant.thrift"
 
 service TakeoutOrderService{
-	restaurant.Restaurant getRestaurant();
+	restaurant.Restaurant getRestaurant(1: string restaurantId);
 	bool versionMatches(1: common.Id version);
 	restaurant.ClientOrderReceipt placeOrder(1: restaurant.OrderPlacedByClient order);
 }
