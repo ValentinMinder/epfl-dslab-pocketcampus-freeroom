c694e6f3a8b6c4c3fabf59c2e598eee20cb1459e
Elodie Triponez <elodienilane.triponez@epfl.ch>
2011-12-13 10:44:40 +0000

    <server> put plugins in alphabetical order
    git-svn-id: file:///var/pocketcampus/trunk@1461 fd477b11-563a-408f-9d6e-eeb9ca009d0c

--- platform/launcher/platform.launcher.server/src/org/pocketcampus/platform/launcher/server/PocketCampusServer.java
+++ platform/launcher/platform.launcher.server/src/org/pocketcampus/platform/launcher/server/PocketCampusServer.java
@@ -47,19 +47,19 @@ public class PocketCampusServer extends ServerBase {
 		// Add all the plugin server implementations here.
 		// The name must match the plugin name given in the plugin controller (and must be unique!)
 		// TODO create a unique id directly in the thrift file when compiling the thrift files?
-		processors.add(new Processor(new MapService.Processor<MapServiceImpl>(new MapServiceImpl()), "map"));
 		processors.add(new Processor(new AuthenticationService.Processor<AuthenticationServiceImpl>(new AuthenticationServiceImpl()), "authentication"));
 		processors.add(new Processor(new BikeService.Processor<BikesServiceImpl>(new BikesServiceImpl()), "bikes"));
 		processors.add(new Processor(new CamiproService.Processor<CamiproServiceImpl>(new CamiproServiceImpl()), "camipro"));
 		processors.add(new Processor(new DirectoryService.Processor<DirectoryServiceImpl>(new DirectoryServiceImpl()), "directory"));
 		processors.add(new Processor(new FoodService.Processor<FoodServiceImpl>(new FoodServiceImpl()), "food"));
+		processors.add(new Processor(new IsacademiaService.Processor<IsacademiaServiceImpl>(new IsacademiaServiceImpl()), "isacademia"));
+		processors.add(new Processor(new MapService.Processor<MapServiceImpl>(new MapServiceImpl()), "map"));
 		processors.add(new Processor(new NewsService.Processor<NewsServiceImpl>(new NewsServiceImpl()), "news"));
 		processors.add(new Processor(new SatelliteService.Processor<SatelliteServiceImpl>(new SatelliteServiceImpl()), "satellite"));
 		processors.add(new Processor(new TakeoutGateway.Processor<TakeoutGatewayImpl>(new TakeoutGatewayImpl()), "takeout-gateway"));
 		processors.add(new Processor(new TakeoutOrderService.Processor<TakeoutOrderServiceImpl>(new TakeoutOrderServiceImpl()), "takeout-order"));
 		processors.add(new Processor(new TakeoutReceiverService.Processor<TakeoutReceiverServiceImpl>(new TakeoutReceiverServiceImpl()), "takeoutreceiver"));
 		processors.add(new Processor(new TransportService.Processor<TransportServiceImpl>(new TransportServiceImpl()), "transport"));
-		processors.add(new Processor(new IsacademiaService.Processor<IsacademiaServiceImpl>(new IsacademiaServiceImpl()), "isacademia"));
 		
 		return processors;
 	}
