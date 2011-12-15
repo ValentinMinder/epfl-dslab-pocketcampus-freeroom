c2d4798f7b45776e9a7dde5f4a09b63c7b74d64b
Elodie Triponez <elodienilane.triponez@epfl.ch>
2011-12-15 09:43:40 +0000

    <food> added fish to the list
    git-svn-id: file:///var/pocketcampus/trunk@1497 fd477b11-563a-408f-9d6e-eeb9ca009d0c

--- plugin/food/plugin.food.android/src/org/pocketcampus/plugin/food/android/utils/MealTagger.java
+++ plugin/food/plugin.food.android/src/org/pocketcampus/plugin/food/android/utils/MealTagger.java
@@ -51,7 +51,7 @@ public class MealTagger {
 		// Pattern.compile(".*(�minc�|roti|rago�t|gigot|escalope|steak|brochette).*",Pattern.CASE_INSENSITIVE|Pattern.MULTILINE);
 		Pattern fishPatterns = Pattern
 				.compile(
-						".*(poisson|carrelet|grenadier|lotte|dorade|chevalier|cabillaud|saumon|pangasius|lieu|bar|mulet|truite|st.?Pierre|colin|perche|rougaille|calamars|hoki|sandre|carpe|raie).*",
+						".*(poisson|carrelet|grenadier|lotte|dorade|chevalier|cabillaud|saumon|pangasius|lieu|^bar$|mulet|truite|st.?Pierre|colin|perche|rougaille|calamars|hoki|sandre|carpe|raie|silure|fl.?tan).*",
 						Pattern.CASE_INSENSITIVE | Pattern.MULTILINE);
 		Pattern vegetarianPatterns = Pattern.compile(".*v.?g.?tarienne.*",
 				Pattern.CASE_INSENSITIVE | Pattern.MULTILINE);
