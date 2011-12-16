7e19f66015409e6dcf67e4b22968ed4bfaaefd55
Elodie Triponez <elodienilane.triponez@epfl.ch>
2011-12-16 11:12:27 +0000

    <events> added server
    git-svn-id: file:///var/pocketcampus/trunk@1516 fd477b11-563a-408f-9d6e-eeb9ca009d0c

--- platform/launcher/platform.launcher.server/src/org/pocketcampus/platform/launcher/server/PocketCampusServer.java
+++ platform/launcher/platform.launcher.server/src/org/pocketcampus/platform/launcher/server/PocketCampusServer.java
@@ -12,6 +12,8 @@ import org.pocketcampus.plugin.camipro.server.CamiproServiceImpl;
 import org.pocketcampus.plugin.camipro.shared.CamiproService;
 import org.pocketcampus.plugin.directory.server.DirectoryServiceImpl;
 import org.pocketcampus.plugin.directory.shared.DirectoryService;
+import org.pocketcampus.plugin.events.server.EventsServiceImpl;
+import org.pocketcampus.plugin.events.shared.EventsService;
 import org.pocketcampus.plugin.food.server.FoodServiceImpl;
 import org.pocketcampus.plugin.food.shared.FoodService;
 import org.pocketcampus.plugin.isacademia.server.IsacademiaServiceImpl;
@@ -22,14 +24,8 @@ import org.pocketcampus.plugin.news.server.NewsServiceImpl;
 import org.pocketcampus.plugin.news.shared.NewsService;
 import org.pocketcampus.plugin.satellite.server.SatelliteServiceImpl;
 import org.pocketcampus.plugin.satellite.shared.SatelliteService;
-import org.pocketcampus.plugin.takeoutreceiver.shared.TakeoutGateway;
-import org.pocketcampus.plugin.takeoutreceiver.shared.TakeoutOrderService;
-import org.pocketcampus.plugin.takeoutreceiver.shared.TakeoutReceiverService;
 import org.pocketcampus.plugin.transport.server.TransportServiceImpl;
 import org.pocketcampus.plugin.transport.shared.TransportService;
-import org.pocketcampus.server.plugin.takeout.TakeoutGatewayImpl;
-import org.pocketcampus.server.plugin.takeout.TakeoutOrderServiceImpl;
-import org.pocketcampus.server.plugin.takeoutreceiver.TakeoutReceiverServiceImpl;
 
 public class PocketCampusServer extends ServerBase {
 
@@ -51,14 +47,15 @@ public class PocketCampusServer extends ServerBase {
 		processors.add(new Processor(new BikeService.Processor<BikesServiceImpl>(new BikesServiceImpl()), "bikes"));
 		processors.add(new Processor(new CamiproService.Processor<CamiproServiceImpl>(new CamiproServiceImpl()), "camipro"));
 		processors.add(new Processor(new DirectoryService.Processor<DirectoryServiceImpl>(new DirectoryServiceImpl()), "directory"));
+		processors.add(new Processor(new EventsService.Processor<EventsServiceImpl>(new EventsServiceImpl()), "events"));
 		processors.add(new Processor(new FoodService.Processor<FoodServiceImpl>(new FoodServiceImpl()), "food"));
 		processors.add(new Processor(new IsacademiaService.Processor<IsacademiaServiceImpl>(new IsacademiaServiceImpl()), "isacademia"));
 		processors.add(new Processor(new MapService.Processor<MapServiceImpl>(new MapServiceImpl()), "map"));
 		processors.add(new Processor(new NewsService.Processor<NewsServiceImpl>(new NewsServiceImpl()), "news"));
 		processors.add(new Processor(new SatelliteService.Processor<SatelliteServiceImpl>(new SatelliteServiceImpl()), "satellite"));
-		processors.add(new Processor(new TakeoutGateway.Processor<TakeoutGatewayImpl>(new TakeoutGatewayImpl()), "takeout-gateway"));
-		processors.add(new Processor(new TakeoutOrderService.Processor<TakeoutOrderServiceImpl>(new TakeoutOrderServiceImpl()), "takeout-order"));
-		processors.add(new Processor(new TakeoutReceiverService.Processor<TakeoutReceiverServiceImpl>(new TakeoutReceiverServiceImpl()), "takeoutreceiver"));
+//		processors.add(new Processor(new TakeoutGateway.Processor<TakeoutGatewayImpl>(new TakeoutGatewayImpl()), "takeout-gateway"));
+//		processors.add(new Processor(new TakeoutOrderService.Processor<TakeoutOrderServiceImpl>(new TakeoutOrderServiceImpl()), "takeout-order"));
+//		processors.add(new Processor(new TakeoutReceiverService.Processor<TakeoutReceiverServiceImpl>(new TakeoutReceiverServiceImpl()), "takeoutreceiver"));
 		processors.add(new Processor(new TransportService.Processor<TransportServiceImpl>(new TransportServiceImpl()), "transport"));
 		
 		return processors;
