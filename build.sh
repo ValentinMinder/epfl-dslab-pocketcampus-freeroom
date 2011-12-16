b23a0fce11f58db062852a192e47005bbc870e83
Florian Laurent <florian.laurent@epfl.ch>
2011-12-16 21:13:27 +0000

    <tool> improved build script: cleans, merges, builds, uninstalls previous version and installs new
    git-svn-id: file:///var/pocketcampus/trunk@1546 fd477b11-563a-408f-9d6e-eeb9ca009d0c

--- build.sh
+++ build.sh
@@ -1,19 +1,54 @@
 #!/bin/bash
-mvn clean 
-cd tool/tool.builder/ 
-ant 
-cd ../../
+
+# timer utility function
+function timer() {
+    if [[ $# -eq 0 ]]; then
+        echo $(date '+%s')
+    else
+        local  stime=$1
+        etime=$(date '+%s')
+
+        if [[ -z "$stime" ]]; then stime=$etime; fi
+
+        dt=$((etime - stime))
+        ds=$((dt % 60))
+        dm=$(((dt / 60) % 60))
+        dh=$((dt / 3600))
+        printf '%02dm%02ds' $dm $ds
+    fi
+}
+
+tmr=$(timer)
+
+# makes sure we got the path to the android sdk
 if [ -z "$ANDROID_HOME" -o ! -z "$1" ]
 then
   if [ -z "$1" ]
   then
-    echo "build <android_home>"
+    echo "Usage: build <android_home>"
     exit 1
   fi 
-  echo "Building with Android home = $1"
+  echo "Using Android SDK from: $1"
   echo "" >> ~/.bashrc
   echo "export ANDROID_HOME=$1" >> ~/.bashrc
   export ANDROID_HOME=$1
 fi
-echo "Starting the build"
+
+echo "Cleaning up…"
+mvn clean 
+
+echo "Merging the plugins..."
+cd tool/tool.builder/ 
+ant 
+cd ../../
+
+echo "Building the application…"
 mvn install
+
+echo "Uninstalling old app…"
+adb uninstall org.pocketcampus
+
+echo "Installing new app..."
+adb install merged/android/target/merged.android-0.3-SNAPSHOT.apk
+
+printf 'All done, took %s\n' $(timer $tmr) 
