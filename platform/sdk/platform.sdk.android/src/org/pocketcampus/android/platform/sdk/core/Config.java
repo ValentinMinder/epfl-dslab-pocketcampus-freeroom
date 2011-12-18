5f7d108a5cdc5fdb251630a890b6648b36196ed8
Florian Laurent <florian.laurent@epfl.ch>
2011-12-18 20:08:44 +0000

    <server> re-enabling runtime configuration load (forgot some imports)
    git-svn-id: file:///var/pocketcampus/trunk@1635 fd477b11-563a-408f-9d6e-eeb9ca009d0c

--- platform/sdk/platform.sdk.android/src/org/pocketcampus/android/platform/sdk/core/Config.java
+++ platform/sdk/platform.sdk.android/src/org/pocketcampus/android/platform/sdk/core/Config.java
@@ -1,5 +1,11 @@
 package org.pocketcampus.android.platform.sdk.core;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+import android.os.Environment;
 import android.util.Log;
 
 /**
