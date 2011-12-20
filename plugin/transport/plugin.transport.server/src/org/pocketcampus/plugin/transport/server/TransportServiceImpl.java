55a7879f1e5bf3ad0f2c1e268efc8a091af54f3c
scheiben <scheiben>
2011-12-20 23:48:08 +0000

    <Transport> No POI in autocomplete result 			null check in getTrips results
    git-svn-id: file:///var/pocketcampus/trunk@1651 fd477b11-563a-408f-9d6e-eeb9ca009d0c

--- plugin/transport/plugin.transport.server/src/org/pocketcampus/plugin/transport/server/TransportServiceImpl.java
+++ plugin/transport/plugin.transport.server/src/org/pocketcampus/plugin/transport/server/TransportServiceImpl.java
@@ -24,50 +24,54 @@ public class TransportServiceImpl implements TransportService.Iface {
 	private SbbProvider mSbbProvider;
 
 	public TransportServiceImpl() {
-		mSbbProvider = new SbbProvider("MJXZ841ZfsmqqmSymWhBPy5dMNoqoGsHInHbWJQ5PTUZOJ1rLTkn8vVZOZDFfSe");
-		
+		mSbbProvider = new SbbProvider(
+				"MJXZ841ZfsmqqmSymWhBPy5dMNoqoGsHInHbWJQ5PTUZOJ1rLTkn8vVZOZDFfSe");
+
 		System.out.println("Transport started.");
-		
-		//testing getLocationsFromIDs
-//		ArrayList<Integer> l = new ArrayList<Integer>();
-//		l.add(new Integer(8501214));
-//		l.add(new Integer(8501215));
-//		l.add(new Integer(8501216));
-//		l.add(new Integer(8501217));
-//		l.add(new Integer(8501218));
-//		l.add(new Integer(8504221));
-//		
-//		try {
-//			for(TransportStation loc : getLocationsFromIDs(l)){
-//				if(loc != null)
-//					System.out.println(loc.name);
-//				else
-//					System.out.println("no corresponding station was found");
-//			}
-//		} catch (TException e) {
-//			System.out.println("something very bad happend, you probably gonna die");
-//		}
-		
+
+		// testing getLocationsFromIDs
+		// ArrayList<Integer> l = new ArrayList<Integer>();
+		// l.add(new Integer(8501214));
+		// l.add(new Integer(8501215));
+		// l.add(new Integer(8501216));
+		// l.add(new Integer(8501217));
+		// l.add(new Integer(8501218));
+		// l.add(new Integer(8504221));
+		//
+		// try {
+		// for(TransportStation loc : getLocationsFromIDs(l)){
+		// if(loc != null)
+		// System.out.println(loc.name);
+		// else
+		// System.out.println("no corresponding station was found");
+		// }
+		// } catch (TException e) {
+		// System.out.println("something very bad happend, you probably gonna die");
+		// }
 
 		try {
-//			//System.out.println(autocomplete("Neuchatel").get(0).id);
+			// //System.out.println(autocomplete("Neuchatel").get(0).id);
 			QueryTripsResult res = getTrips("EPFL", "Neuchatel");
-//			QueryTripsResult res = getTripsFromStationsIDs("8501214", "8504221");
-//			System.out.println("from "+ res.from.name + " to " + res.to.name);
-//			for(TransportTrip tt : res.connections){
-//				System.out.println(new Date(tt.departureTime));
-//			}
-//			// EPFL -> Neuchatel
-//			
-//			
-//			
-			//testing newDepartures
-//			QueryDepartureResult q = nextDepartures("8501214");
-//			for(StationDepartures s :q.stationDepartures){
-//				for(Departure d : s.departures){
-//					System.out.println(d.destination + " with " + d.line + " at " + (new Date(d.plannedTime)).toString());
-//				}
-//			}
+			res.toString();
+			// QueryTripsResult res = getTripsFromStationsIDs("8501214",
+			// "8504221");
+			// System.out.println("from "+ res.from.name + " to " +
+			// res.to.name);
+			// for(TransportTrip tt : res.connections){
+			// System.out.println(new Date(tt.departureTime));
+			// }
+			// // EPFL -> Neuchatel
+			//
+			//
+			//
+			// testing newDepartures
+			// QueryDepartureResult q = nextDepartures("8501214");
+			// for(StationDepartures s :q.stationDepartures){
+			// for(Departure d : s.departures){
+			// System.out.println(d.destination + " with " + d.line + " at " +
+			// (new Date(d.plannedTime)).toString());
+			// }
+			// }
 		} catch (TException e1) {
 			e1.printStackTrace();
 		}
@@ -75,8 +79,8 @@ public class TransportServiceImpl implements TransportService.Iface {
 	}
 
 	@Override
-	public List<TransportStation> autocomplete(String constraint) throws TException {
-		System.out.println("autocomplete");
+	public List<TransportStation> autocomplete(String constraint)
+			throws TException {
 
 		List<de.schildbach.pte.dto.Location> sbbCompletions = null;
 		try {
@@ -87,33 +91,39 @@ public class TransportServiceImpl implements TransportService.Iface {
 
 		List<TransportStation> completions = new ArrayList<TransportStation>();
 		for (de.schildbach.pte.dto.Location location : sbbCompletions) {
-			completions.add(new TransportStation(TransportStationType.ANY, location.id,
-					location.lat, location.lon, location.place, location.name));
+			// this is to get only Stations and nothing else
+			if(location.type == LocationType.STATION)
+				completions.add(new TransportStation(TransportStationType.ANY,
+					location.id, location.lat, location.lon, location.place,
+					location.name));
 		}
 
 		return completions;
 	}
 
 	@Override
-	public List<TransportStation> getLocationsFromNames(List<String> names) throws TException {
+	public List<TransportStation> getLocationsFromNames(List<String> names)
+			throws TException {
 		ArrayList<TransportStation> locList = new ArrayList<TransportStation>();
-		
-		for(String name: names){
+
+		for (String name : names) {
 			try {
-				TransportStation loc = SchildbachToPCConverter.convertSchToPC(mSbbProvider.autocompleteStations(name).get(0));
+				TransportStation loc = SchildbachToPCConverter
+						.convertSchToPC(mSbbProvider.autocompleteStations(name)
+								.get(0));
 				locList.add(loc);
 			} catch (IOException e) {
 				System.out.println("could not get stations from name: " + name);
 			}
 		}
-		
+
 		return locList;
 	}
-	
-	
+
 	/**
-	 *Returns a TransportStation list with the stations corresponding to the integers id list of the param 
-	 * if an id has not been found, the corresponding TransportStation in the result will be null
+	 * Returns a TransportStation list with the stations corresponding to the
+	 * integers id list of the param if an id has not been found, the
+	 * corresponding TransportStation in the result will be null
 	 * 
 	 * DOES NOT WORK FOR NOW, SHOULD TRY WITH THE UPDATE OF THE SCHILDBACH SDK
 	 */
@@ -127,28 +137,29 @@ public class TransportServiceImpl implements TransportService.Iface {
 				de.schildbach.pte.dto.Location sLocation = new de.schildbach.pte.dto.Location(
 						de.schildbach.pte.dto.LocationType.STATION,
 						inte.intValue());
-				NearbyStationsResult res = mSbbProvider.queryNearbyStations(sLocation, 100000, 5);
+				NearbyStationsResult res = mSbbProvider.queryNearbyStations(
+						sLocation, 100000, 5);
 
-				
 				if (res != null) {
 					boolean found = false;
-					
-					List<TransportStation> ts_list = SchildbachToPCConverter.convertSchToPC(res.stations);
+
+					List<TransportStation> ts_list = SchildbachToPCConverter
+							.convertSchToPC(res.stations);
 					System.out.println(res.stations.size());
-					for(TransportStation loc: ts_list)
-					{
-						if(loc.id == inte.intValue()){
+					for (TransportStation loc : ts_list) {
+						if (loc.id == inte.intValue()) {
 							System.out.println(loc);
 							found = true;
 							locations.add(loc);
 							break;
 						}
-							
+
 					}
-					
-					if(!found){
+
+					if (!found) {
 						locations.add(null);
-						System.out.println(inte.intValue() + " has not been found");
+						System.out.println(inte.intValue()
+								+ " has not been found");
 					}
 				} else {
 					System.out.println(res);
@@ -156,8 +167,7 @@ public class TransportServiceImpl implements TransportService.Iface {
 			}
 
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println("IO Exception with schildbach");
 		}
 		return locations;
 	}
@@ -173,9 +183,10 @@ public class TransportServiceImpl implements TransportService.Iface {
 		QueryDepartureResult nextDepartures = null;
 
 		try {
-			
-			nextDepartures = SchildbachToPCConverter.convertSchToPC(mSbbProvider.queryDepartures(
-					Integer.parseInt(IDStation), 5, false));
+
+			nextDepartures = SchildbachToPCConverter
+					.convertSchToPC(mSbbProvider.queryDepartures(
+							Integer.parseInt(IDStation), 5, false));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -187,34 +198,20 @@ public class TransportServiceImpl implements TransportService.Iface {
 	public QueryTripsResult getTrips(String from, String to) throws TException {
 
 		long now = (new Date()).getTime();
-		QueryTripsResult tmp = getTripsFromSchildbach(from, to, now, true);
+		return getTripsFromSchildbach(from, to, now, true);
 		
-		for(TransportTrip tt: tmp.getConnections()){
-			for(TransportConnection tc : tt.getParts()){
-				tc.setFootIsSet(true);
-				
-				if(tc.foot == false) {
-					tc.setArrivalTimeIsSet(true);
-					tc.setDepartureTimeIsSet(true);
-					tc.setLineIsSet(true);
-					
-				} else {
-					tc.setMinIsSet(true);
-				}
-			}
-		}
-		
-		return tmp;
 	}
 
 	@Override
-	public QueryTripsResult getTripsAtTime(String from, String to, long time, boolean isDeparture) throws TException {
-		
+	public QueryTripsResult getTripsAtTime(String from, String to, long time,
+			boolean isDeparture) throws TException {
+
 		return getTripsFromSchildbach(from, to, time, isDeparture);
 	}
-	
-	private QueryTripsResult getTripsFromSchildbach(String from, String to, long time, boolean isDeparture){
-		
+
+	private QueryTripsResult getTripsFromSchildbach(String from, String to,
+			long time, boolean isDeparture) {
+
 		if (from == null || to == null) {
 			return null;
 		}
@@ -241,33 +238,80 @@ public class TransportServiceImpl implements TransportService.Iface {
 
 		QueryTripsResult tripResults = null;
 		try {
-			tripResults = SchildbachToPCConverter.convertSchToPC(mSbbProvider.queryConnections(fromLoc,
-					viaLoc, toLoc, date, isDeparture, products, walkSpeed));
+			tripResults = SchildbachToPCConverter.convertSchToPC(mSbbProvider
+					.queryConnections(fromLoc, viaLoc, toLoc, date,
+							isDeparture, products, walkSpeed));
+			
+			
+			
+			
+			if (tripResults.getConnections() != null) {
+				for (TransportTrip tt : tripResults.getConnections()) {
+					for (TransportConnection tc : tt.getParts()) {
+						tc.setFootIsSet(true);
+
+						if (tc.foot == false) {
+							tc.setArrivalTimeIsSet(true);
+							tc.setDepartureTimeIsSet(true);
+							tc.setLineIsSet(true);
+
+						} else {
+							tc.setMinIsSet(true);
+						}
+					}
+				}
+			}else if(tripResults.from == null && tripResults.to == null){
+				tripResults = null;
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
+		
+		
+		
+		
 		return tripResults;
 	}
-	
 
 	@Override
-	public QueryTripsResult getTripsFromStationsIDs(String fromID,
-			String toID) throws TException {
+	public QueryTripsResult getTripsFromStationsIDs(String fromID, String toID)
+			throws TException {
 
 		de.schildbach.pte.dto.Location fromLoc = null, viaLoc = null, toLoc = null;
 		fromLoc = new Location(LocationType.STATION, Integer.parseInt(fromID));
-		toLoc   = new Location(LocationType.STATION, Integer.parseInt(toID));
-		
+		toLoc = new Location(LocationType.STATION, Integer.parseInt(toID));
+
 		QueryTripsResult tripResults = null;
 		try {
 			String products = (String) null;
 			WalkSpeed walkSpeed = WalkSpeed.NORMAL;
-			tripResults = SchildbachToPCConverter.convertSchToPC(mSbbProvider.queryConnections(fromLoc, viaLoc, toLoc, new Date(), true, products, walkSpeed));
+			tripResults = SchildbachToPCConverter.convertSchToPC(mSbbProvider
+					.queryConnections(fromLoc, viaLoc, toLoc, new Date(), true,
+							products, walkSpeed));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
-		
+		if (tripResults.getConnections() != null) {
+			for (TransportTrip tt : tripResults.getConnections()) {
+				for (TransportConnection tc : tt.getParts()) {
+					tc.setFootIsSet(true);
+
+					if (tc.foot == false) {
+						tc.setArrivalTimeIsSet(true);
+						tc.setDepartureTimeIsSet(true);
+						tc.setLineIsSet(true);
+
+					} else {
+						tc.setMinIsSet(true);
+					}
+				}
+			}
+		}else if(tripResults.from == null && tripResults.to == null){
+			tripResults = null;
+		}
+
 		return tripResults;
 	}
 
