57828f3eb64ad5216e24d9b186b6edc14c0f7adf
Amer Chamseddine <amer.chamseddine@epfl.ch>
2012-01-15 17:39:42 +0000

    <isacademia> forgot to commit the thrift definition file <isacademia> temporarily add it in the final app
    git-svn-id: file:///var/pocketcampus/trunk@1729 fd477b11-563a-408f-9d6e-eeb9ca009d0c

--- tool/tool.thriftcompiler/service/isacademia.thrift
+++ tool/tool.thriftcompiler/service/isacademia.thrift
@@ -12,8 +12,12 @@ enum SeanceType {
 	SEANCE_CONFLICT; // CONFLIT;
 }
 
+struct IsaRequest {
+	1: required authentication.SessionId iSessionId;
+	2: required string iLanguage;
+}
 
-struct Course {
+struct IsaCourse {
 	1: required string name;
 	2: required string code;
 	3: required string instructor;
@@ -22,7 +26,7 @@ struct Course {
 	6: required i32 credits;
 }
 
-struct Exam {
+struct IsaExam {
 	1: required string course;
 	2: required string code;
 	3: required string instructor;
@@ -34,7 +38,7 @@ struct Exam {
 	9: required string academicYear;
 }
 
-struct Seance {
+struct IsaSeance {
 	1: required string course;
 	2: required string instructor;
 	3: required SeanceType type;
@@ -43,8 +47,23 @@ struct Seance {
 	6: required string room;
 }
 
+struct IsaCoursesListReply {
+	1: optional list<IsaCourse> iCourses;
+	2: required i32 iStatus;
+}
+
+struct IsaExamsListReply {
+	1: optional list<IsaExam> iExams;
+	2: required i32 iStatus;
+}
+
+struct IsaScheduleReply {
+	1: optional list<IsaSeance> iSeances;
+	2: required i32 iStatus;
+}
+
 service IsacademiaService {
-	list<Course> getUserCourses(1: authentication.SessionId aSessionId);
-	list<Exam> getUserExams(1: authentication.SessionId aSessionId);
-	list<Seance> getUserSchedule(1: authentication.SessionId aSessionId);
+	IsaCoursesListReply getUserCourses(1: IsaRequest iRequest);
+	IsaExamsListReply getUserExams(1: IsaRequest iRequest);
+	IsaScheduleReply getUserSchedule(1: IsaRequest iRequest);
 }
