c41ea229157060904b03bd6dc4d0e490aed252f3
Amer Chamseddine <amer.chamseddine@epfl.ch>
2012-02-25 20:16:22 +0000

    <general> tool to easily create new plugins based on another plugin as a template
    git-svn-id: file:///var/pocketcampus/trunk@1741 fd477b11-563a-408f-9d6e-eeb9ca009d0c

---
+++ plugin/create_new_plugin_from_template.php
@@ -0,0 +1,106 @@
+<?php
+
+$template_plugin_dir = "/home/amer/epfl/svn/pocketcampus/trunk/plugin/moodle";
+$new_plugin_dir = "/home/amer/epfl/svn/pocketcampus/trunk/plugin/sunrise";
+
+$replacements = array("moodle" => "sunrise", "Moodle" => "Sunrise", "MOODLE" => "SUNRISE");
+
+$to_delete_dirs = array(".svn");
+$to_empty_dirs = array("bin", "gen");
+
+$deleted_dirs = array();
+$emptied_dirs = array();
+$processed_dirs = array();
+$processed_files = array();
+$weird_cases = array();
+
+function process_dir($dir) {
+
+	global $replacements;
+	
+	global $deleted_dirs;
+	global $emptied_dirs;
+	global $processed_dirs;
+	global $processed_files;
+	global $weird_cases;
+	
+	global $to_delete_dirs;
+	global $to_empty_dirs;
+	
+	if ($handle = opendir($dir)) {
+		while (false !== ($entry = readdir($handle))) {
+			if($entry != "." && $entry != "..") {
+			
+				//echo "$dir/$entry\n";
+				if(is_file("$dir/$entry")) {
+					$m_entry = $entry;
+					foreach($replacements as $k => $v) {
+						$m_entry = implode($v, explode($k, $m_entry));
+					}
+					if($entry != $m_entry)
+						system("mv $dir/$entry $dir/$m_entry");
+					process_file("$dir/$m_entry");
+					$processed_files[] = "$dir/$m_entry";
+				} elseif(is_dir("$dir/$entry")) {
+					if(in_array($entry, $to_delete_dirs)) {
+						system("rm -Rf $dir/$entry");
+						$deleted_dirs[] = "$dir/$entry";
+					} elseif(in_array($entry, $to_empty_dirs)) {
+						system("rm -Rf $dir/$entry");
+						system("mkdir $dir/$entry");
+						$emptied_dirs[] = "$dir/$entry";
+					} else {
+						$m_entry = $entry;
+						foreach($replacements as $k => $v) {
+							$m_entry = implode($v, explode($k, $m_entry));
+						}
+						if($entry != $m_entry)
+							system("mv $dir/$entry $dir/$m_entry");
+						process_dir("$dir/$m_entry");
+						$processed_dirs[] = "$dir/$m_entry";
+					}
+				} else {
+					$weird_cases[] = "$dir/$entry";
+				}
+			
+			}
+		}
+		closedir($handle);
+	}
+
+}
+
+function process_file($file) {
+	global $replacements;
+	$contents = file_get_contents($file);
+	foreach($replacements as $k => $v) {
+		$contents = implode($v, explode($k, $contents));
+	}
+	file_put_contents($file, $contents);
+}
+
+
+// ================ LOGIC ================ \\
+
+
+if(is_dir($new_plugin_dir) || is_file($new_plugin_dir)) {
+	echo "ERROR `$new_plugin_dir` already exists\n";
+	exit;
+}
+
+system("cp -R $template_plugin_dir $new_plugin_dir");
+
+process_dir($new_plugin_dir);
+
+echo "================= deleted_dirs ======================\n";
+print_r($deleted_dirs);
+echo "================= emptied_dirs ======================\n";
+print_r($emptied_dirs);
+echo "================= processed_dirs ======================\n";
+print_r($processed_dirs);
+echo "================= processed_files ======================\n";
+print_r($processed_files);
+echo "================= weird_cases ======================\n";
+print_r($weird_cases);
+
+?>
