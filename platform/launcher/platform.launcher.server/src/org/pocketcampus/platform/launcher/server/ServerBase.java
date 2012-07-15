fedf2bb0b72d69ec307f1756706f2cd8b970a0a3
Amer Chamseddine <amer.chamseddine@epfl.ch>
2012-07-15 12:15:17 +0000

    <server> now we have an HTTPS certificate
    git-svn-id: file:///var/pocketcampus/trunk@1882 fd477b11-563a-408f-9d6e-eeb9ca009d0c

--- platform/launcher/platform.launcher.server/src/org/pocketcampus/platform/launcher/server/ServerBase.java
+++ platform/launcher/platform.launcher.server/src/org/pocketcampus/platform/launcher/server/ServerBase.java
@@ -5,16 +5,22 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.ArrayList;
+import java.util.LinkedList;
+
 import org.apache.thrift.TProcessor;
 import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.protocol.TProtocolFactory;
 import org.apache.thrift.server.TServlet;
+import org.eclipse.jetty.http.ssl.SslContextFactory;
+import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.NCSARequestLog;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.ContextHandlerCollection;
 import org.eclipse.jetty.server.handler.HandlerCollection;
 import org.eclipse.jetty.server.handler.RequestLogHandler;
+import org.eclipse.jetty.server.nio.SelectChannelConnector;
+import org.eclipse.jetty.server.ssl.SslSelectChannelConnector;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
@@ -27,13 +33,41 @@ public abstract class ServerBase {
 	 * A SAMPLE OF THE FILE IS IN THIS PROJECT'S ROOT DIRECTORY
 	 */
 	
-	public static int LISTEN_ON_PORT = 443;
+	public static int LISTEN_ON_PORT = 9090;
+	public static int SSL_LISTEN_ON_PORT = 0;
 	
 	private static TProtocolFactory protocolFactory = new TBinaryProtocol.Factory();
 	
 	public void start() throws Exception {
-		Server server = new Server(LISTEN_ON_PORT);
+		Server server = new Server();
 		
+        LinkedList<Connector> conn_list = new LinkedList<Connector>();
+
+        SelectChannelConnector connector0 = new SelectChannelConnector();
+        connector0.setPort(LISTEN_ON_PORT);
+        connector0.setMaxIdleTime(30000);
+        connector0.setRequestHeaderSize(8192);
+        conn_list.add(connector0);
+
+        /*SelectChannelConnector connector1 = new SelectChannelConnector();
+        connector1.setHost("127.0.0.1");
+        connector1.setPort(7070);
+        connector1.setThreadPool(new QueuedThreadPool(20));
+        connector1.setName("admin");
+        conn_list.add(connector1);*/
+
+        if(SSL_LISTEN_ON_PORT != 0) {
+	        SslSelectChannelConnector ssl_connector = new SslSelectChannelConnector();
+	        ssl_connector.setPort(SSL_LISTEN_ON_PORT);
+	        SslContextFactory cf = ssl_connector.getSslContextFactory();
+	        cf.setKeyStore("ssl-keystore-pocketcampus");
+	        cf.setKeyStorePassword("pocketcampus");
+	        cf.setKeyManagerPassword("pocketcampus");
+	        conn_list.add(ssl_connector);
+        }
+
+        server.setConnectors((Connector[]) conn_list.toArray(new Connector[conn_list.size()]));
+        
 		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
 		context.setContextPath("/v3r1");
 		
@@ -81,11 +115,14 @@ public abstract class ServerBase {
 					if(param.length == 2) {
 						if("LISTEN_ON_PORT".equals(param[0]))
 							LISTEN_ON_PORT = Integer.parseInt(param[1]);
+						if("SSL_LISTEN_ON_PORT".equals(param[0]))
+							SSL_LISTEN_ON_PORT = Integer.parseInt(param[1]);
 					}
 				}
 			} else {
 				FileWriter fw = new FileWriter(configFile, false);
 				fw.write("LISTEN_ON_PORT=" + LISTEN_ON_PORT + "\n");
+				fw.write("SSL_LISTEN_ON_PORT=" + SSL_LISTEN_ON_PORT + "\n");
 				fw.close();
 			}
 		} catch (Exception e) {
