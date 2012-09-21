919258c3d4022bdf060ad57617f78d042686f243
Amer Chamseddine <amer.chamseddine@epfl.ch>
2012-09-21 20:22:28 +0200

    delete transport lib and server launcher

--- platform/launcher/platform.launcher.server/src/org/pocketcampus/platform/launcher/server/ServerLauncher.java
+++
@@ -1,9 +0,0 @@
-package org.pocketcampus.platform.launcher.server;
-
-
-public class ServerLauncher {
-	public static void main(String[] args) throws Exception {
-		ServerBase server = new PocketCampusServer();
-		server.start();
-	}
-}
