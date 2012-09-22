9cffb6b432040c0816cc99f1cacf6fa0d260b744
Loic Gardiol <loic.gardiol@gmail.com>
2012-09-22 16:40:23 +0200

    iOS : <Directory> : - refractored view to display person, now dedicated class for it - <Authentication> - fix a bug, nav bar was not visible if password had to been asked to user after a password change - fix bug, login button disabled if user scrolled very very far and hide it <Map> - can now see go from Map to Directory if search result is a person - editing search field now removes previous search results <News> popup to ask whether a link should be followed is not titled with link <Food> meals will now refresh if app is taken from background after more than 4 hours

---
+++ ios/iphone/PocketCampus/Plugins/DirectoryPlugin/Controllers/PCUnkownPersonViewController.h
@@ -0,0 +1,13 @@
+//
+//  PCUnkownPersonViewController.h
+//  PocketCampus
+//
+//  Created by Loïc Gardiol on 22.09.12.
+//  Copyright (c) 2012 EPFL. All rights reserved.
+//
+
+#import <AddressBookUI/AddressBookUI.h>
+
+@interface PCUnkownPersonViewController : ABUnknownPersonViewController
+
+@end
