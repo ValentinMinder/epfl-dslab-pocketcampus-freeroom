1500f29c1b87c6ce68d736dde6ef5e7cf5e1e103
Amer Chamseddine <amer.chamseddine@epfl.ch>
2012-10-26 14:33:06 +0200

    [map.server] make it run as standalone

---
+++ plugin/map/plugin.map.server/src/org/pocketcampus/plugin/map/server/PluginLauncher.java
@@ -0,0 +1,25 @@
+package org.pocketcampus.plugin.map.server;
+
+import java.util.ArrayList;
+
+import org.pocketcampus.platform.launcher.server.Processor;
+import org.pocketcampus.platform.launcher.server.ServerBase;
+import org.pocketcampus.plugin.map.shared.MapService;
+
+
+public class PluginLauncher {
+	
+	public static class PocketCampusServer extends ServerBase {
+		protected ArrayList<Processor> getServiceProcessors() {
+			ArrayList<Processor> processors = new ArrayList<Processor>();
+			processors.add(new Processor(new MapService.Processor<MapServiceImpl>(new MapServiceImpl()), "map"));
+			return processors;
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		ServerBase server = new PocketCampusServer();
+		server.start();
+	}
+	
+}
