3a30910b1e688e004affc301b7ec0999f7cb4bc9
Amer Chamseddine <amer@accandme.com>
2013-10-29 16:48:28 +0100

    [food.server] add ServerLauncher

---
+++ plugin/food/plugin.food.server/src/org/pocketcampus/platform/launcher/server/ServerLauncher.java
@@ -0,0 +1,9 @@
+package org.pocketcampus.platform.launcher.server;
+
+public class ServerLauncher {
+	
+	public static void main(String[] args) throws Exception {
+		new PocketCampusServer().start();
+	}
+	
+}
