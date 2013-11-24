264abbffa382d54b33a998e172362fcb12ed6dfd
Loic Gardiol <loic.gardiol@gmail.com>
2013-11-24 09:05:35 +0100

    [transport.ios] model refactoring

---
+++ ios/PocketCampus/Plugins/TransportPlugin/Model/TransportStation+Additions.m
@@ -0,0 +1,61 @@
+//
+//  TransportStation+Additions.m
+//  PocketCampus
+//
+//  Created by Loïc Gardiol on 23.11.13.
+//  Copyright (c) 2013 EPFL. All rights reserved.
+//
+
+#import "TransportStation+Additions.h"
+
+@implementation TransportStation (Additions)
+
+- (NSString*)shortName {
+    if ([self.name isEqualToString:@"Ecublens VD, EPFL"]) {
+        return @"EPFL";
+    }
+    
+    if ([self.name isEqualToString:@"Ecublens VD, EPFL Piccard"]) {
+        return @"EPFL Piccard";
+    }
+    
+    if ([self.name isEqualToString:@"Ecublens VD, UNIL-Sorge"]) {
+        return @"UNIL-Sorge";
+    }
+    
+    if ([self.name isEqualToString:@"Lausanne, Vigie"]) {
+        return @"Vigie";
+    }
+    
+    if ([self.name isEqualToString:@"Chavannes-p.-R., UNIL-Dorigny"]) {
+        return @"UNIL-Dorigny";
+    }
+    
+    if ([self.name isEqualToString:@"Chavannes-p.-R., UNIL-Mouline"]) {
+        return @"UNIL-Mouline";
+    }
+    
+    return self.name;
+}
+
+- (BOOL)isEqual:(id)object {
+    if (self == object) {
+        return YES;
+    }
+    if (![object isKindOfClass:[self class]]) {
+        return NO;
+    }
+    return [self isEqualToTransportStation:object];
+}
+
+- (BOOL)isEqualToTransportStation:(TransportStation*)transportStation {
+    return self.id == transportStation.id;
+}
+
+- (NSUInteger)hash {
+    NSUInteger hash = 0;
+    hash += self.id;
+    return hash;
+}
+
+@end
