d445ef9739385a1cdccbdfc406e0973a4feadeb2
Loic Gardiol <loic.gardiol@gmail.com>
2013-12-01 01:22:25 +0100

    [general.ios] new cooler splash screen

--- ios/PocketCampus/PocketCampus/Views/Main/PCSplashView.m
+++ ios/PocketCampus/PocketCampus/Views/Main/PCSplashView.m
@@ -10,7 +10,11 @@
 
 @interface PCSplashView ()
 
-@property (nonatomic, strong) UIImageView* imageView;
+@property (nonatomic, strong) UIImageView* backgroundImageView;
+
+@property (nonatomic, strong) UIImageView* drawingImageView;
+
+@property (nonatomic, strong) NSLayoutConstraint* drawingImageViewCenterYConstraint;
 
 @end
 
@@ -24,11 +28,18 @@
         self.translatesAutoresizingMaskIntoConstraints = NO;
         [superview addSubview:self];
         [superview addConstraints:[NSLayoutConstraint constraintsToSuperview:superview forView:self edgeInsets:UIEdgeInsetsMake(0, 0, 0, 0)]];
-        self.imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 1, 1)];
-        self.imageView.translatesAutoresizingMaskIntoConstraints = NO;
-        self.imageView.image = [self imageForDevice];
-        [self addSubview:self.imageView];
-        [self addConstraints:[NSLayoutConstraint constraintsToSuperview:self forView:self.imageView edgeInsets:UIEdgeInsetsMake(0, 0, 0, 0)]];
+        self.backgroundImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 1, 1)];
+        self.backgroundImageView.translatesAutoresizingMaskIntoConstraints = NO;
+        self.backgroundImageView.image = [self imageForDevice];
+        [self addSubview:self.backgroundImageView];
+        [self addConstraints:[NSLayoutConstraint constraintsToSuperview:self forView:self.backgroundImageView edgeInsets:UIEdgeInsetsMake(0, 0, 0, 0)]];
+        
+        self.drawingImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"PocketCampusDrawing"]];
+        self.drawingImageView.translatesAutoresizingMaskIntoConstraints = NO;
+        [self.backgroundImageView addSubview:self.drawingImageView];
+        self.drawingImageViewCenterYConstraint = [NSLayoutConstraint constraintForCenterYtoSuperview:self.backgroundImageView forView:self.drawingImageView constant:0.0];
+        [self.backgroundImageView addConstraint:self.drawingImageViewCenterYConstraint];
+        [self.backgroundImageView addConstraint:[NSLayoutConstraint constraintForCenterXtoSuperview:self.backgroundImageView forView:self.drawingImageView constant:0.0]];
         
         
     }
@@ -43,12 +54,19 @@
 #pragma mark - Public methods
 
 - (void)hideWithAnimationDuration:(NSTimeInterval)duration completion:(VoidBlock)completion {
-    [UIView animateWithDuration:duration animations:^{
-        self.alpha = 0.0;
+    self.drawingImageViewCenterYConstraint.constant = 40.0;
+    [UIView animateWithDuration:duration*0.6 delay:0.0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+        [self.backgroundImageView layoutIfNeeded];
     } completion:^(BOOL finished) {
-        if (completion) {
-            completion();
-        }
+        self.drawingImageViewCenterYConstraint.constant = [PCUtils is4inchDevice] ? -300.0 : -220.0;
+        [UIView animateWithDuration:duration*0.4 delay:0.0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+            self.alpha = 0.0;
+            [self.backgroundImageView layoutIfNeeded];
+        } completion:^(BOOL finished) {
+            if (completion) {
+                completion();
+            }
+        }];
     }];
 }
 
