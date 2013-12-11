e22addc3fc208441c2a8576d4e3396a80ff9c8b3
Loic Gardiol <loic.gardiol@gmail.com>
2013-12-11 17:07:15 +0100

    [news] updated definition to not use common.h (removed from other plugins definition in a previous commit)

--- plugin/news/plugin.news.shared/def/news.thrift
+++ plugin/news/plugin.news.shared/def/news.thrift
@@ -1,22 +1,17 @@
 namespace java org.pocketcampus.plugin.news.shared
 namespace csharp org.pocketcampus.plugin.news.shared
 
-include "../../../../platform/sdk/platform.sdk.shared/def/common.thrift"
-
-typedef i32 int
-typedef i64 timestamp
-
 struct NewsItem {
-	1: required common.Id newsItemId;
+	1: required i64 newsItemId;
 	2: required string title;
 	3: required string link;
 	4: required string feed;
-	5: required timestamp pubDate;
+	5: required i64 pubDate;
 	6: optional string imageUrl;
 }
 
 struct Feed {
-	1: required common.Id feedId;
+	1: required i64 feedId;
 	2: required string title;
 	3: required string link;
 	4: required string description;
@@ -25,7 +20,7 @@ struct Feed {
 
 service NewsService {
 	list<NewsItem> getNewsItems(1: string language);
-	string getNewsItemContent(1: common.Id newsItemId);
+	string getNewsItemContent(1: i64 newsItemId);
 	map<string, string> getFeedUrls(1: string language);
 	list<Feed> getFeeds(1: string language);
 }
