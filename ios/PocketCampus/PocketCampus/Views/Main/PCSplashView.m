8b535ef791dbd7ccf512c0a5180060c2597745c5
Loic Gardiol <loic.gardiol@gmail.com>
2014-01-07 17:11:26 +0100

    [general.ios] refactoring; updated CocoaPods to 0.29.0

--- ios/PocketCampus/PocketCampus/Views/Main/PCSplashView.m
+++ ios/PocketCampus/PocketCampus/Views/Main/PCSplashView.m
@@ -58,30 +58,33 @@
 }
 
 - (void)hideWithAnimationDelay:(NSTimeInterval)delay duration:(NSTimeInterval)duration completion:(VoidBlock)completion; {
-    //duration = 15.0;
+    //duration = 5.0;
     
     CGFloat originalHeight = self.drawingImageView.frame.size.height;
     CGFloat compressedHeight = self.drawingImageView.frame.size.height*0.85;
     CGFloat originalCenterYConstraintConstant = self.drawingImageViewCenterYConstraint.constant;
     NSLayoutConstraint* compressedHeightConstraint = [NSLayoutConstraint heightConstraint:compressedHeight forView:self.drawingImageView];
+    NSTimeInterval m1Duration = duration*0.55;
+    NSTimeInterval m2Duration = duration*0.12;
+    NSTimeInterval m3Duration = duration*0.33;
     
     [self.drawingImageView addConstraint:compressedHeightConstraint];
     self.drawingImageViewCenterYConstraint.constant = (originalHeight-compressedHeight)/2.0;
     
-    [UIView animateWithDuration:duration*0.6 delay:delay options:UIViewAnimationOptionCurveEaseInOut animations:^{
+    [UIView animateWithDuration:m1Duration delay:delay options:UIViewAnimationOptionCurveEaseInOut animations:^{
         [self.backgroundImageView layoutIfNeeded];
     } completion:^(BOOL finished) {
         
         [self.drawingImageView removeConstraint:compressedHeightConstraint];
         self.drawingImageViewCenterYConstraint.constant = originalCenterYConstraintConstant;
         
-        [UIView animateWithDuration:duration*0.07 delay:0.0 options:UIViewAnimationOptionCurveEaseIn animations:^{
+        [UIView animateWithDuration:m2Duration delay:0.0 options:UIViewAnimationOptionCurveEaseIn animations:^{
             [self.backgroundImageView layoutIfNeeded];
         } completion:^(BOOL finished) {
             
             self.drawingImageViewCenterYConstraint.constant = [PCUtils is4inchDevice] ? -500.0 : -412.0;
-            
-            [UIView animateWithDuration:duration*0.33 delay:0.0 options:UIViewAnimationOptionCurveLinear animations:^{
+            //NSLog(@"M3 avg speed: %lf", self.drawingImageViewCenterYConstraint.constant/m3Duration);
+            [UIView animateWithDuration:duration*m3Duration delay:0.0 options:UIViewAnimationOptionCurveLinear animations:^{
                 self.alpha = 0.0;
                 [self.backgroundImageView layoutIfNeeded];
             } completion:^(BOOL finished) {
