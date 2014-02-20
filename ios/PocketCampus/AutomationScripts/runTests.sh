66de00acfbfb622a3e1415ba63c13769dd15f90e
silviu <silviu@pocketcampus.org>
2014-02-20 11:31:58 +0100

    PocketCampus/ios/PocketCampus/AutomationScripts $ ./runTests.sh /tmp/PocketCampus.app `pwd`/PocketCampusAutoMonkey.js `pwd` 86cd8769d1d484d07428581e30e9cb515be9c5d1

--- ios/PocketCampus/AutomationScripts/runTests.sh
+++ ios/PocketCampus/AutomationScripts/runTests.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
 
-APP_NAME="$1"
-echo "APP_NAME=${APP_NAME}"
-DIR=`find /Users/${USER}/Library/Application\ Support/iPhone\ Simulator/7.0.3/Applications/ -name "${APP_NAME}"`; echo $DIR; DIR=`dirname "$DIR"`; echo $DIR; rm -rf "$DIR"
+if [ $# -eq 0 ]
+then
+	echo "usage: runTest.sh APP_NAME(full path to name.app) SCRIPT(full path to script) RESULT PATH(full path) DEVICE IDENTIFIER"
+	exit
+fi
 
-XCODE_DIR=`dirname ${DEVELOPER_DIR}`
+APP_NAME="$1"
+DEVICE_IDENTIFIER="$4"
+echo "APP_NAME=${APP_NAME}"
+
+if [ -z "${DEVELOPER_DIR}" ]
+	then
+	XCODE_DIR=`xcode-select --print-path`
+	XCODE_DIR=`dirname ${XCODE_DIR}`
+else
+	XCODE_DIR=`dirname ${DEVELOPER_DIR}`
+fi
+echo "XCODE_DIR=${XCODE_DIR}"
+
 TEMPLATE="{XCODE_DIR}/Applications/Instruments.app/Contents/PlugIns/AutomationInstrument.bundle/Contents/Resources/Automation.tracetemplate"
 
-echo "About to run: ${XCODE_DIR}/Developer/usr/bin/instruments -t ${XCODE_DIR}/Applications/Instruments.app/Contents/PlugIns/AutomationInstrument.bundle/Contents/Resources/Automation.tracetemplate /tmp/${APP_NAME} -e UIASCRIPT $2 -e UIARESULTSPATH $3"
-${XCODE_DIR}/Developer/usr/bin/instruments -t ${XCODE_DIR}/Applications/Instruments.app/Contents/PlugIns/AutomationInstrument.bundle/Contents/Resources/Automation.tracetemplate /tmp/${APP_NAME} -e UIASCRIPT $2 -e UIARESULTSPATH $3
+if [ -z "${DEVICE_IDENTIFIER}" ]
+	then
+	echo "Removing the app from the simulator"
+	DIR=`find /Users/${USER}/Library/Application\ Support/iPhone\ Simulator/7.0.3/Applications/ -name "${APP_NAME}"`; 
+	echo $DIR; 
+	DIR=`dirname "$DIR"`; 
+	echo $DIR; 
+	rm -rf "$DIR"
+	${XCODE_DIR}/Developer/usr/bin/instruments -t ${XCODE_DIR}/Applications/Instruments.app/Contents/PlugIns/AutomationInstrument.bundle/Contents/Resources/Automation.tracetemplate /tmp/${APP_NAME} -e UIASCRIPT $2 -e UIARESULTSPATH $3
+else
+	${XCODE_DIR}/Developer/usr/bin/instruments -t ${XCODE_DIR}/Applications/Instruments.app/Contents/PlugIns/AutomationInstrument.bundle/Contents/Resources/Automation.tracetemplate -w "${DEVICE_IDENTIFIER}" ${APP_NAME} -e UIASCRIPT $2 -e UIARESULTSPATH $3
+fi
