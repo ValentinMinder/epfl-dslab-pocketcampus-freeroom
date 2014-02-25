3017272b5b567100291cb8437f1faca605528901
Julien Weber <julien.weber@epfl.ch>
2014-02-25 17:17:26 +0100

    [freeroom.server] Changing thrift file

--- plugin/freeroom/plugin.freeroom.shared/def/freeroom.thrift
+++ plugin/freeroom/plugin.freeroom.shared/def/freeroom.thrift
@@ -1,5 +1,27 @@
 namespace java org.pocketcampus.plugin.freeroom.shared
 namespace csharp org.pocketcampus.plugin.freeroom.shared
 
+struct PeriodOfTime{
+	1: required Day day;
+	2: required i32 startHour;
+	3: required i32 endHour;
+}
+
+struct Room{
+	1: required string building;
+	2: required string number;
+	3: optionnal RoomType type;
+	4: optionnal i32 capacity;
+}
+
+enum Day{
+	MONDAY; TUESDAY; WEDNESDAY; THURSDAY; FRIDAY; SATURDAY; SUNDAY;
+}
+
+enum RoomType{
+	AUDITORIUM; EXERCISES;
+}
+
 service FreeRoomService {
+	set<Room> getFreeRoomsFromTime(1: PeriodOfTime period);
 }
