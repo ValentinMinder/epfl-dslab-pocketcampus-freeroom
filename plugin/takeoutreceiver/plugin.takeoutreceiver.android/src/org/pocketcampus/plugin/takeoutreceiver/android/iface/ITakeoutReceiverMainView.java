5c48b3e2b71e66e857275c52118c485f870e474c
Solal Pirelli <solal.pirelli@gmail.com>
2014-02-26 15:12:40 +0100

    Removed takeout and takeoutreceived (fixes #114)

--- plugin/takeoutreceiver/plugin.takeoutreceiver.android/src/org/pocketcampus/plugin/takeoutreceiver/android/iface/ITakeoutReceiverMainView.java
+++
@@ -1,7 +0,0 @@
-package org.pocketcampus.plugin.takeoutreceiver.android.iface;
-
-import org.pocketcampus.android.platform.sdk.core.IView;
-
-public interface ITakeoutReceiverMainView extends IView {
-	public void ordersUpdated();
-}
