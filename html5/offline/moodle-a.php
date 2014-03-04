f6e529af2513d818397f9c80133e824bf5c4012c
Solal Pirelli <solal.pirelli@gmail.com>
2014-03-04 18:42:03 +0100

    Removed html5 (fixes #116)

--- html5/offline/moodle-a.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once "common.php";
-
-echo "<!DOCTYPE HTML>\n<html manifest=\"" . getUrl("moodle-b.php") . "\"><head><meta http-equiv=\"refresh\" content=\"1;URL='" . getUrl("moodle.php") . "'\"></head><body></body></html>";
-
-?>
