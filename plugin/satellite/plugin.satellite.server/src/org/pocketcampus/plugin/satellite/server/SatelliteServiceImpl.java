437e7407601b392055c6442c4c181b5fdc592fe1
Solal Pirelli <solal.pirelli@gmail.com>
2014-03-05 19:39:12 +0100

    [satellite.server] Fixed the error introduced by the last commit

--- plugin/satellite/plugin.satellite.server/src/org/pocketcampus/plugin/satellite/server/SatelliteServiceImpl.java
+++ plugin/satellite/plugin.satellite.server/src/org/pocketcampus/plugin/satellite/server/SatelliteServiceImpl.java
@@ -14,6 +14,8 @@ import org.joda.time.Duration;
  * @author Solal Pirelli <solal.pirelli@epfl.ch>
  */
 public final class SatelliteServiceImpl implements SatelliteService.Iface {
+	private static final Duration MENU_CACHE_DURATION = Duration.standardDays(1);
+	
 	private final BeerMenu _beerMenu;
 
 	public SatelliteServiceImpl(BeerMenu beerMenu) {
@@ -21,7 +23,7 @@ public final class SatelliteServiceImpl implements SatelliteService.Iface {
 	}
 
 	public SatelliteServiceImpl() {
-		this(CachingProxy.create(new BeerMenuImpl(new HttpClientImpl()), Duration.standardDays(1)));
+		this(CachingProxy.create(new BeerMenuImpl(new HttpClientImpl()), MENU_CACHE_DURATION, false));
 	}
 
 	@Override
