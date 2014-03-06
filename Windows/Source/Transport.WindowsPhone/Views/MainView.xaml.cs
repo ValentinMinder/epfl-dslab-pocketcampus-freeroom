4b5a416aa847501ca67a609add4af59f3dc4b3bd
Solal Pirelli <solal.pirelli@gmail.com>
2014-03-06 17:23:01 +0100

    [general.wp] Bugfixes.

--- Windows/Source/Transport.WindowsPhone/Views/MainView.xaml.cs
+++ Windows/Source/Transport.WindowsPhone/Views/MainView.xaml.cs
@@ -2,10 +2,13 @@
 // See LICENSE file for more details
 // File author: Solal Pirelli
 
+using System;
 using System.Windows;
 using System.Windows.Data;
 using Microsoft.Phone.Controls;
 using PocketCampus.Common.Controls;
+using PocketCampus.Mvvm;
+using PocketCampus.Transport.ViewModels;
 
 namespace PocketCampus.Transport.Views
 {
@@ -16,14 +19,36 @@ namespace PocketCampus.Transport.Views
             InitializeComponent();
         }
 
+        // HACK: The ListPicker is awful, it can't bind to SelectedItem when the source is null
         private void StationsPicker_Loaded( object sender, RoutedEventArgs e )
         {
-            var binding = new Binding
+            Action bindSelectedItem = () =>
             {
-                Path = new PropertyPath( "SelectedStation" ),
-                Mode = BindingMode.TwoWay
+                var binding = new Binding
+                {
+                    Path = new PropertyPath( "SelectedStation" ),
+                    Mode = BindingMode.TwoWay
+                };
+                StationsPicker.SetBinding( ListPicker.SelectedItemProperty, binding );
             };
-            StationsPicker.SetBinding( ListPicker.SelectedItemProperty, binding );
+
+            var vm = (MainViewModel) DataContext;
+            if ( vm.SelectedStation == null )
+            {
+                bool ok = false;
+                vm.ListenToProperty( x => x.SelectedStation, () =>
+                {
+                    if ( !ok )
+                    {
+                        bindSelectedItem();
+                        ok = true;
+                    }
+                } );
+            }
+            else
+            {
+                bindSelectedItem();
+            }
         }
     }
 }
