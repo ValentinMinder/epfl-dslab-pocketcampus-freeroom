c71d630ed4614aa39af7b56feba7b308901429ee
Solal Pirelli <solal.pirelli@gmail.com>
2014-03-25 21:22:22 +0100

    [general.wp] Vector icons where possible & better icons (from ModernUiIcons.com) everywhere else

--- Windows/Source/Common.WindowsPhone/Converters.cs
+++ Windows/Source/Common.WindowsPhone/Converters.cs
@@ -6,33 +6,11 @@ using System;
 using System.Globalization;
 using System.Resources;
 using System.Windows;
-using System.Windows.Data;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace PocketCampus.Common
 {
-    /// <summary>
-    /// Base converter class that adds type safety and reduces the number of parameters
-    /// (since most converters don't use them).
-    /// </summary>
-    public abstract class ValueConverter<TFrom, TTo> : IValueConverter
-    {
-        public object Convert( object value, Type targetType, object parameter, CultureInfo culture )
-        {
-            return Convert( (TFrom) value );
-        }
-
-        public object ConvertBack( object value, Type targetType, object parameter, CultureInfo culture )
-        {
-            return ConvertBack( (TTo) value );
-        }
-
-        public abstract TTo Convert( TFrom value );
-        public virtual TFrom ConvertBack( TTo value ) { throw new NotSupportedException(); }
-    }
-
-
     /// <summary>
     /// Converts enums to localized strings.
     /// </summary>
