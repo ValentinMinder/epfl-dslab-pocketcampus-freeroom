ValentinMinder/epfl-dslab-pocketcampus-freeroom

commit 4cb557a95d6934cdd3bcff4d5b83cb2b69e10860

[freeroom.android] Refactoring adapters of expandable list views

plugin/freeroom/plugin.freeroom.android/src/org/pocketcampus/plugin/freeroom/android/adapter/ExpandableAbstractListViewAdapter.java
@@ -1,0 +1,119 @@
+package org.pocketcampus.plugin.freeroom.android.adapter;
+
+import java.util.List;
+import java.util.Map;
+
+import org.pocketcampus.plugin.freeroom.android.FreeRoomModel;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+
+public abstract class ExpandableAbstractListViewAdapter<T> extends
+		BaseExpandableListAdapter {
+	protected Context context;
+	protected List<String> headers;
+	protected Map<String, List<T>> data;
+	// hold the caller view for colors updates.
+	protected FreeRoomModel mModel;
+
+	public ExpandableAbstractListViewAdapter(Context c, List<String> header,
+			Map<String, List<T>> data, FreeRoomModel model) {
+		this.context = c;
+		this.headers = header;
+		this.data = data;
+		this.mModel = model;
+	}
+	
+	public ExpandableAbstractListViewAdapter(Context c, List<String> header,
+			Map<String, List<T>> data) {
+		this.context = c;
+		this.headers = header;
+		this.data = data;
+		this.mModel = null;
+	}
+
+	/**
+	 * Return the corresponding child's doorCode, this method is intented for
+	 * the display, thus should not return the door UID, if you want the object
+	 * FRRoom, use getChildObject instead
+	 */
+	@Override
+	abstract public Object getChild(int groupPosition, int childPosition);
+
+	/**
+	 * This method returns the child's object. It is not suitable for display,
+	 * use getChild(int, int) instead.
+	 * 
+	 * @param groupPosition
+	 *            The group id
+	 * @param childPosition
+	 *            The child id
+	 * @return The child object FRRoom associated.
+	 */
+	abstract public T getChildObject(int groupPosition, int childPosition);
+
+	@Override
+	public long getChildId(int groupPosition, int childPosition) {
+		return childPosition;
+	}
+
+	@Override
+	abstract public View getChildView(int groupPosition, int childPosition,
+			boolean isLastChild, View convertView, ViewGroup parent);
+
+	@Override
+	public int getChildrenCount(int groupPosition) {
+		if (groupPosition >= data.size()) {
+			return 0;
+		}
+		List<?> list = data.get(headers.get(groupPosition));
+		if (list != null) {
+			return list.size();
+		}
+		return 0;
+	}
+
+	@Override
+	public Object getGroup(int groupPosition) {
+		if (groupPosition >= data.size()) {
+			return null;
+		}
+		return data.get(headers.get(groupPosition));
+	}
+
+	@Override
+	public int getGroupCount() {
+		return data.size();
+	}
+
+	@Override
+	public long getGroupId(int groupPosition) {
+		return groupPosition;
+	}
+
+	@Override
+	abstract public View getGroupView(int groupPosition, boolean isExpanded,
+			View convertView, ViewGroup parent);
+
+	@Override
+	public boolean hasStableIds() {
+		return false;
+	}
+
+	@Override
+	public boolean isChildSelectable(int groupPosition, int childPosition) {
+		return true;
+	}
+
+	public void updateHeader(int id, String value) {
+		if (id >= headers.size()) {
+			return;
+		}
+
+		this.data.put(value, data.remove(headers.get(id)));
+		this.headers.set(id, value);
+	}
+
+}

plugin/freeroom/plugin.freeroom.android/res/values/freeroom_strings.xml
@@ -22,6 +22,7 @@
     <string name="freeroom_menu_freeroom_now">Find me a free room now !</string>
     <string name="freeroom_menu_freeroom_search">Search for a free room</string>
     <string name="freeroom_menu_check_occupancy">Check the occupancy</string>
+    <string name="freeroom_menu_manage_favorites">Manage favorites</string>
     <string name="freeroom_searchbutton">Search !</string>
     <string name="freeroom_resetbutton">Reset !</string>
     <string name="freeroom_selectday">Day</string>

plugin/freeroom/plugin.freeroom.android/src/org/pocketcampus/plugin/freeroom/android/FreeRoomManageFavoritesView.java
@@ -1,0 +1,77 @@
+package org.pocketcampus.plugin.freeroom.android;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+import org.pocketcampus.android.platform.sdk.core.PluginController;
+import org.pocketcampus.android.platform.sdk.tracker.Tracker;
+import org.pocketcampus.android.platform.sdk.ui.layout.StandardTitledLayout;
+import org.pocketcampus.plugin.freeroom.R;
+import org.pocketcampus.plugin.freeroom.android.adapter.ExpandableSimpleListViewAdapter;
+import org.pocketcampus.plugin.freeroom.android.iface.IFreeRoomView;
+
+import android.os.Bundle;
+import android.widget.ExpandableListView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class FreeRoomManageFavoritesView extends FreeRoomAbstractView implements
+		IFreeRoomView {
+
+	private FreeRoomController mController;
+	private FreeRoomModel mModel;
+
+	private StandardTitledLayout mLayout;
+	private LinearLayout subLayout;
+
+	private TextView mTitle;
+
+	private ExpandableListView mExpList;
+	private ExpandableSimpleListViewAdapter mAdapter;
+
+	@Override
+	protected Class<? extends PluginController> getMainControllerClass() {
+		return FreeRoomController.class;
+	}
+
+	@Override
+	protected void onDisplay(Bundle savedInstanceState,
+			PluginController controller) {
+		// Tracker
+		Tracker.getInstance().trackPageView("freeroom/checkoccupancy/result");
+
+		// Get and cast the controller and model
+		mController = (FreeRoomController) controller;
+		mModel = (FreeRoomModel) controller.getModel();
+
+		// Setup the layout
+		mLayout = new StandardTitledLayout(this);
+
+		// The ActionBar is added automatically when you call setContentView
+		setContentView(mLayout);
+		mLayout.setTitle(getString(R.string.freeroom_title_occupancy_result));
+
+		// launch the actual search AFTER launching completely the UI
+		mController.checkOccupancy(this);
+	}
+
+	@Override
+	public void freeRoomResultsUpdated() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void autoCompletedUpdated() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void occupancyResultUpdated() {
+		// TODO Auto-generated method stub
+		
+	}
+}

plugin/freeroom/plugin.freeroom.android/src/org/pocketcampus/plugin/freeroom/android/adapter/ExpandableListViewFavoriteAdapter.java
@@ -25,26 +25,13 @@ import android.widget.TextView;
  * 
  */
 public class ExpandableListViewFavoriteAdapter extends
-		BaseExpandableListAdapter {
-
-	private Context context;
-	private List<String> headers;
-	private Map<String, List<FRRoom>> data;
-	private FreeRoomModel model;
+		ExpandableAbstractListViewAdapter<FRRoom> {
 
 	public ExpandableListViewFavoriteAdapter(Context c, List<String> header,
 			Map<String, List<FRRoom>> data, FreeRoomModel model) {
-		this.context = c;
-		this.headers = header;
-		this.data = data;
-		this.model = model;
+		super(c, header, data, model);
 	}
 
-	/**
-	 * Return the corresponding child's doorCode, this method is intented for
-	 * the display, thus should not return the door UID, if you want the object
-	 * FRRoom, use getChildObject instead
-	 */
 	@Override
 	public Object getChild(int groupPosition, int childPosition) {
 		FRRoom child = this.getChildObject(groupPosition, childPosition);
@@ -54,34 +41,6 @@ public class ExpandableListViewFavoriteAdapter extends
 		return null;
 	}
 
-	/**
-	 * This method returns the child's object. It is not suitable for display,
-	 * use getChild(int, int) instead.
-	 * 
-	 * @param groupPosition
-	 *            The group id
-	 * @param childPosition
-	 *            The child id
-	 * @return The child object FRRoom associated.
-	 */
-	public FRRoom getChildObject(int groupPosition, int childPosition) {
-		if (groupPosition >= headers.size()) {
-			return null;
-		}
-		List<FRRoom> groupList = data.get(headers.get(groupPosition));
-
-		if (childPosition >= groupList.size() || groupList == null) {
-			return null;
-		}
-		return groupList.get(childPosition);
-	}
-
-	@Override
-	public long getChildId(int groupPosition, int childPosition) {
-		return childPosition;
-	}
-
-	@Override
 	public View getChildView(int groupPosition, int childPosition,
 			boolean isLastChild, View convertView, ViewGroup parent) {
 
@@ -105,17 +64,18 @@ public class ExpandableListViewFavoriteAdapter extends
 
 		final FRRoom room = this.getChildObject(groupPosition, childPosition);
 		// after all, this may be useless...
-		final FRRoom mFRRoom = model.getFreeRoomResult(groupPosition, childPosition);
+		final FRRoom mFRRoom = mModel.getFreeRoomResult(groupPosition,
+				childPosition);
 
 		TextView tv = vholder.getTextView();
 		tv.setText(room.getDoorCode());
 		// TODO: Julien: this is commented because it cause the app to crash!
-//		ImageView map = vholder.getImageViewMap();
-//		map.setImageResource(android.R.drawable.btn_plus);
+		// ImageView map = vholder.getImageViewMap();
+		// map.setImageResource(android.R.drawable.btn_plus);
 		final ImageView star = vholder.getImageViewStar();
 
 		final String uid = room.getUid();
-		final boolean isFav = model.containRoomFavorites(uid);
+		final boolean isFav = mModel.containRoomFavorites(uid);
 
 		if (isFav) {
 			star.setImageResource(android.R.drawable.star_big_on);
@@ -130,46 +90,16 @@ public class ExpandableListViewFavoriteAdapter extends
 			public void onClick(View v) {
 				if (isFav) {
 					star.setImageResource(android.R.drawable.star_big_off);
-					model.removeRoomFavorites(room.getUid());
+					mModel.removeRoomFavorites(room.getUid());
 				} else {
 					star.setImageResource(android.R.drawable.star_big_on);
-					model.addRoomFavorites(room.getUid(), room.getDoorCode());
+					mModel.addRoomFavorites(room.getUid(), room.getDoorCode());
 				}
 				notifyDataSetChanged();
 			}
 		});
 		vholder.setStarCheck(false);
 		return convertView;
-	}
-
-	@Override
-	public int getChildrenCount(int groupPosition) {
-		if (groupPosition >= data.size()) {
-			return 0;
-		}
-		List<FRRoom> list = data.get(headers.get(groupPosition));
-		if (list != null) {
-			return list.size();
-		}
-		return 0;
-	}
-
-	@Override
-	public Object getGroup(int groupPosition) {
-		if (groupPosition >= data.size()) {
-			return null;
-		}
-		return data.get(headers.get(groupPosition));
-	}
-
-	@Override
-	public int getGroupCount() {
-		return data.size();
-	}
-
-	@Override
-	public long getGroupId(int groupPosition) {
-		return groupPosition;
 	}
 
 	@Override
@@ -196,25 +126,6 @@ public class ExpandableListViewFavoriteAdapter extends
 		tv.setText(text);
 		return convertView;
 
-	}
-
-	@Override
-	public boolean hasStableIds() {
-		return false;
-	}
-
-	@Override
-	public boolean isChildSelectable(int groupPosition, int childPosition) {
-		return true;
-	}
-
-	public void updateHeader(int id, String value) {
-		if (id >= headers.size()) {
-			return;
-		}
-
-		this.data.put(value, data.remove(headers.get(id)));
-		this.headers.set(id, value);
 	}
 
 	/**
@@ -278,6 +189,12 @@ public class ExpandableListViewFavoriteAdapter extends
 			return this.tv;
 		}
 
+	}
+
+	@Override
+	public FRRoom getChildObject(int groupPosition, int childPosition) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }

plugin/freeroom/plugin.freeroom.android/src/org/pocketcampus/plugin/freeroom/android/adapter/ExpandableSimpleListViewAdapter.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.pocketcampus.plugin.freeroom.R;
 import org.pocketcampus.plugin.freeroom.android.FreeRoomModel;
+import org.pocketcampus.plugin.freeroom.shared.FRRoom;
 
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -21,44 +22,17 @@ import android.widget.TextView;
  *         Valentin MINDER <valentin.minder@epfl.ch>
  * 
  */
-public class ExpandableSimpleListViewAdapter extends BaseExpandableListAdapter {
-	private Context context;
-	private List<String> headers;
-	private Map<String, List<String>> data;
-	// hold the caller view for colors updates.
-	private FreeRoomModel mModel;
-
-	public ExpandableSimpleListViewAdapter(Context c, List<String> header,
-			Map<String, List<String>> data) {
-		this.context = c;
-		this.headers = header;
-		this.data = data;
-	}
+public class ExpandableSimpleListViewAdapter extends
+		ExpandableAbstractListViewAdapter<String> {
 
 	public ExpandableSimpleListViewAdapter(Context c, List<String> header,
 			Map<String, List<String>> data, FreeRoomModel model) {
-		this.context = c;
-		this.headers = header;
-		this.data = data;
-		this.mModel = model;
+		super(c, header, data, model);
 	}
 
-	@Override
-	public Object getChild(int groupPosition, int childPosition) {
-		if (groupPosition >= headers.size()) {
-			return null;
-		}
-		List<String> groupList = data.get(headers.get(groupPosition));
-
-		if (childPosition >= groupList.size()) {
-			return null;
-		}
-		return data.get(headers.get(groupPosition)).get(childPosition);
-	}
-
-	@Override
-	public long getChildId(int groupPosition, int childPosition) {
-		return childPosition;
+	public ExpandableSimpleListViewAdapter(Context c, List<String> header,
+			Map<String, List<String>> data) {
+		super(c, header, data);
 	}
 
 	@Override
@@ -93,33 +67,6 @@ public class ExpandableSimpleListViewAdapter extends BaseExpandableListAdapter {
 	}
 
 	@Override
-	public int getChildrenCount(int groupPosition) {
-		if (groupPosition >= data.size()) {
-			return 0;
-		}
-
-		return data.get(headers.get(groupPosition)).size();
-	}
-
-	@Override
-	public Object getGroup(int groupPosition) {
-		if (groupPosition >= data.size()) {
-			return null;
-		}
-		return data.get(headers.get(groupPosition));
-	}
-
-	@Override
-	public int getGroupCount() {
-		return data.size();
-	}
-
-	@Override
-	public long getGroupId(int groupPosition) {
-		return groupPosition;
-	}
-
-	@Override
 	public View getGroupView(int groupPosition, boolean isExpanded,
 			View convertView, ViewGroup parent) {
 		if (groupPosition >= headers.size()) {
@@ -141,42 +88,22 @@ public class ExpandableSimpleListViewAdapter extends BaseExpandableListAdapter {
 		String text = (String) headers.get(groupPosition);
 		TextView tv = vholder.getTextView();
 		tv.setText(text);
-		// The color is set only when it's not expanded as a summary of the content
+		// The color is set only when it's not expanded as a summary of the
+		// content
 		// Occupied, Free or Free/Occupied
 		// Otherwise it's defaut color
 		if (mModel != null) {
 			ExpandableListView v = ((ExpandableListView) parent);
-			if(v.isGroupExpanded(groupPosition)) {
-				convertView.setBackgroundColor(mModel.COLOR_CHECK_OCCUPANCY_DEFAULT);
+			if (v.isGroupExpanded(groupPosition)) {
+				convertView
+						.setBackgroundColor(mModel.COLOR_CHECK_OCCUPANCY_DEFAULT);
 			} else {
-				convertView.setBackgroundColor(mModel.getColorOfCheckOccupancyRoom(
-						groupPosition));
+				convertView.setBackgroundColor(mModel
+						.getColorOfCheckOccupancyRoom(groupPosition));
 			}
 		}
 		return convertView;
 
-	}
-
-	@Override
-	public boolean hasStableIds() {
-		return false;
-	}
-
-	@Override
-	public boolean isChildSelectable(int groupPosition, int childPosition) {
-		if (mModel != null) {
-			return mModel.isCheckOccupancyLineClickable(groupPosition, childPosition);
-		}
-		return true;
-	}
-
-	public void updateHeader(int id, String value) {
-		if (id >= headers.size()) {
-			return;
-		}
-
-		this.data.put(value, data.remove(headers.get(id)));
-		this.headers.set(id, value);
 	}
 
 	/**
@@ -194,5 +121,23 @@ public class ExpandableSimpleListViewAdapter extends BaseExpandableListAdapter {
 		public TextView getTextView() {
 			return this.tv;
 		}
+	}
+
+	@Override
+	public Object getChild(int groupPosition, int childPosition) {
+		if (groupPosition >= headers.size()) {
+			return null;
+		}
+		List<String> groupList = data.get(headers.get(groupPosition));
+
+		if (childPosition >= groupList.size()) {
+			return null;
+		}
+		return data.get(headers.get(groupPosition)).get(childPosition);
+	}
+
+	@Override
+	public String getChildObject(int groupPosition, int childPosition) {
+		return (String) getChild(groupPosition, childPosition);
 	}
 }

commit 0030c9122a275e9d74ee5109bf5ad22de120b491

[freeroom.android] correction of bug (making an occupied room selectable) introduced by commit 4cb557a95d6934cdd3bcff4d5b83cb2b69e10860

plugin/freeroom/plugin.freeroom.android/src/org/pocketcampus/plugin/freeroom/android/adapter/ExpandableAbstractListViewAdapter.java
@@ -25,7 +25,7 @@ public abstract class ExpandableAbstractListViewAdapter<T> extends
 		this.data = data;
 		this.mModel = model;
 	}
-	
+
 	public ExpandableAbstractListViewAdapter(Context c, List<String> header,
 			Map<String, List<T>> data) {
 		this.context = c;
@@ -104,6 +104,10 @@ public abstract class ExpandableAbstractListViewAdapter<T> extends
 
 	@Override
 	public boolean isChildSelectable(int groupPosition, int childPosition) {
+		if (mModel != null) {
+			return mModel.isCheckOccupancyLineClickable(groupPosition,
+					childPosition);
+		}
 		return true;
 	}
 