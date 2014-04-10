6155846ca2c775779066cace1534e128e05cedce
ValentinMinder <valentin.minder@epfl.ch>
2014-04-10 16:15:55 +0200

    Revert "[freeroom.android] using old server with new UI (to be deleted)"
    This reverts commit ef403888923aed487bd202be72b32630636e99c0.

--- plugin/freeroom/plugin.freeroom.android/src/org/pocketcampus/plugin/freeroom/android/FreeRoomController.java
+++ plugin/freeroom/plugin.freeroom.android/src/org/pocketcampus/plugin/freeroom/android/FreeRoomController.java
@@ -1,10 +1,8 @@
 package org.pocketcampus.plugin.freeroom.android;
 
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Map;
 
 import org.pocketcampus.android.platform.sdk.core.PluginController;
 import org.pocketcampus.android.platform.sdk.core.PluginModel;
@@ -131,14 +129,6 @@ public class FreeRoomController extends PluginController implements
 	}
 
 	public void setCheckOccupancyResults(OccupancyReply result) {
-
-		// ONLY for testing new interface with old data!
-		Map<String, List<Occupancy>> map = new HashMap<String, List<Occupancy>>();
-		System.out.println(result.getOccupancyOfRooms().size()
-				+ " size of rooms av.");
-		map.put("BC", result.getOccupancyOfRooms());
-		mModel.setOccupancyResults(map);
-
 		List<Occupancy> list = result.getOccupancyOfRooms();
 
 		// this check the uniqueness of the rooms in the reply
