bc6b4e2c022ac849d4ff8f5a793a678ad5f1b4b5
Julien Weber <julien.weber@epfl.ch>
2014-04-25 20:03:57 +0200

    [freeroom.server] Fixing bug in the convertion to the next half hour

--- plugin/freeroom/plugin.freeroom.server/src/org/pocketcampus/plugin/freeroom/server/utils/Utils.java
+++ plugin/freeroom/plugin.freeroom.server/src/org/pocketcampus/plugin/freeroom/server/utils/Utils.java
@@ -22,17 +22,18 @@ import org.pocketcampus.plugin.freeroom.shared.FreeRoomRequest;
  * 
  */
 public class Utils {
-	
+
 	// TODO: move in FRTimes in shared!!!
 	public static final long m30s_IN_MS = 30 * 1000;
 	public static final long ONE_HOUR_MS = 60 * 60 * 1000;
 	public static final long m30M_MS = 60 * 30 * 1000;
 	public static final long ONE_DAY_MS = ONE_HOUR_MS * 24;
 	public static final long MARGIN_ERROR = 5 * 60 * 1000;
+
 	/**
-	 * Adjust the period given in the request. It adds 30s to the lower
-	 * bound, substract 30s from the upper bound. It is used to allow a
-	 * margin for error with the timestamps
+	 * Adjust the period given in the request. It adds 30s to the lower bound,
+	 * substract 30s from the upper bound. It is used to allow a margin for
+	 * error with the timestamps
 	 * 
 	 * @param req
 	 *            The intial request issued by the client
@@ -46,16 +47,17 @@ public class Utils {
 		return new FreeRoomRequest(period);
 
 	}
-	
+
 	public static String extractBuilding(String doorCode) {
 		String mDoorCode = doorCode.trim();
 		int firstSpace = mDoorCode.indexOf(" ");
 		if (firstSpace > 0) {
 			mDoorCode = mDoorCode.substring(0, firstSpace);
 		} else {
-			Pattern mBuildingPattern = Pattern.compile("^([A-Za-z]+)[^A-Za-z]$");
+			Pattern mBuildingPattern = Pattern
+					.compile("^([A-Za-z]+)[^A-Za-z]$");
 			Matcher mMatcher = mBuildingPattern.matcher(doorCode);
-			
+
 			if (mMatcher.matches()) {
 				return mMatcher.group(0);
 			} else {
@@ -64,8 +66,9 @@ public class Utils {
 		}
 		return mDoorCode;
 	}
-	
-	public static Map<String, List<FRRoom>> sortRoomsByBuilding(List<FRRoom> rooms) {
+
+	public static Map<String, List<FRRoom>> sortRoomsByBuilding(
+			List<FRRoom> rooms) {
 		Iterator<FRRoom> iter = rooms.iterator();
 		HashMap<String, List<FRRoom>> sortedResult = new HashMap<String, List<FRRoom>>();
 		ArrayList<String> buildingsList = new ArrayList<String>();
@@ -86,35 +89,35 @@ public class Utils {
 
 		return sortedResult;
 	}
-	
+
 	public static long roundHourBefore(long timestamp) {
 		long min = timestamp % ONE_HOUR_MS;
 		return timestamp - min;
 	}
 
-	//TODO one half a hour is added mysteriously from the converter probably
+	// TODO one half a hour is added mysteriously from the converter probably
 	public static long roundHourAfter(long timestamp) {
 		long minToCompleteHour = ONE_HOUR_MS - (timestamp % ONE_HOUR_MS);
-		if (minToCompleteHour >= ONE_HOUR_MS - MARGIN_ERROR || minToCompleteHour == ONE_HOUR_MS) {
+		if (minToCompleteHour >= ONE_HOUR_MS - MARGIN_ERROR
+				|| minToCompleteHour == ONE_HOUR_MS) {
 			return timestamp;
 		}
 		return timestamp + minToCompleteHour;
 	}
-	
+
 	public static long determineNumberHour(long tsStart, long tsEnd) {
 		long startHour = Utils.roundHourBefore(tsStart);
 		long endHour = Utils.roundHourAfter(tsEnd);
 
-		
 		Calendar mCalendar = Calendar.getInstance();
 		mCalendar.setTimeInMillis(startHour);
 		int start = mCalendar.get(Calendar.HOUR_OF_DAY);
 		mCalendar.setTimeInMillis(endHour);
 		int end = mCalendar.get(Calendar.HOUR_OF_DAY);
-				
+
 		return end - start;
 	}
-	
+
 	public static long roundToNearestHalfHourBefore(long timestamp) {
 		long timeToCompleteHour = ONE_HOUR_MS - timestamp % ONE_HOUR_MS;
 
@@ -128,25 +131,30 @@ public class Utils {
 
 	public static long roundToNearestHalfHourAfter(long timestamp) {
 		long timeToCompleteHour = ONE_HOUR_MS - timestamp % ONE_HOUR_MS;
+		long timeInMin = timestamp % ONE_HOUR_MS;
 		
-		//if the hour is full (like 8:00am) no need to round
-		if (timeToCompleteHour >= ONE_HOUR_MS - MARGIN_ERROR || timeToCompleteHour == ONE_HOUR_MS) {
+		// if the hour is full (like 8:00am) no need to round
+		if (timeInMin == 0) {
 			return timestamp;
+		} else if (timeInMin < MARGIN_ERROR && timeInMin > 0) {
+			return timestamp - timeInMin;
 		}
 
-		//if we are beyond 30min for the hour, we take the next one
+		// if we are beyond 30min for the hour, we take the next one
 		if (timeToCompleteHour < m30M_MS) {
 			return timestamp + timeToCompleteHour;
 		}
 
-		//otherwise we take the next half hour
+		// otherwise we take the next half hour
 		long timeInMinToHalfHour = m30M_MS - timestamp % m30M_MS;
 		return timestamp + timeInMinToHalfHour;
 	}
 
 	/**
 	 * Set the seconds and milliseconds to zero in the given timestamp.
-	 * @param timeStamp The timestamp to round
+	 * 
+	 * @param timeStamp
+	 *            The timestamp to round
 	 * @return The timestamp rounded.
 	 */
 	public static long roundSAndMSToZero(long timeStamp) {
