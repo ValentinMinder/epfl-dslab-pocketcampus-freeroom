fe3c6ee5a90df56332c1571e69433a49baa7f246
Julien Weber <julien.weber@epfl.ch>
2014-05-14 00:20:33 +0200

    [freeroom.server] Adding fetch method for json

--- plugin/freeroom/plugin.freeroom.server/src/org/pocketcampus/plugin/freeroom/data/FetchOccupancyDataJSON.java
+++ plugin/freeroom/plugin.freeroom.server/src/org/pocketcampus/plugin/freeroom/data/FetchOccupancyDataJSON.java
@@ -1,15 +1,25 @@
 package org.pocketcampus.plugin.freeroom.data;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URISyntaxException;
 import java.util.Scanner;
 
+import org.apache.http.HttpException;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.pocketcampus.platform.sdk.server.database.ConnectionManager;
 import org.pocketcampus.platform.sdk.server.database.handlers.exceptions.ServerException;
 import org.pocketcampus.plugin.freeroom.server.FreeRoomServiceImpl;
+import org.pocketcampus.plugin.freeroom.shared.utils.FRTimes;
 
 public class FetchOccupancyDataJSON {
 	private final String URL_DATA = "https://isatest.epfl.ch/services/timetable/reservations/";
@@ -92,5 +102,42 @@ public class FetchOccupancyDataJSON {
 			e.printStackTrace();
 			return null;
 		}
+	}
+	
+	private String fetch(long timestamp) {
+		String timestampString = FRTimes.convertTimeStampInString(timestamp);
+		System.out.println("Start fetching for ... " + timestampString);
+		DefaultHttpClient client = new DefaultHttpClient();
+		HttpGet request;
+		try {
+			request = new HttpGet(URL_DATA + timestampString);
+			request.addHeader("Accept", "application/json");
+
+			HttpResponse response = client.execute(request);
+
+			if (response.getStatusLine().getStatusCode() == HttpURLConnection.HTTP_OK) {
+
+				BufferedReader reader = new BufferedReader(
+						new InputStreamReader(response.getEntity().getContent()));
+
+				StringBuffer jsonBuffer = new StringBuffer();
+				String line = "";
+				while ((line = reader.readLine()) != null) {
+					jsonBuffer.append(line);
+				}
+				System.out.println("Successfully fetched from server");
+				return jsonBuffer.toString();
+			} else {
+				System.err.println("Error while fetching ,status  "
+						+ response.getStatusLine().getStatusCode());
+			}
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		} catch (HttpException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
