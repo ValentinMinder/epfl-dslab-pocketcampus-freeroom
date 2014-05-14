0d902c3d1e0a7c261734a11bf691999b008bf6fc
Julien Weber <julien.weber@epfl.ch>
2014-05-14 19:42:51 +0200

    [freeroom.server] Adding skeleton of the Runnable for periodically update

---
+++ plugin/freeroom/plugin.freeroom.server/src/org/pocketcampus/plugin/freeroom/data/PeriodicallyUpdate.java
@@ -0,0 +1,16 @@
+package org.pocketcampus.plugin.freeroom.data;
+/**
+Runnable used to be launched periodically to fetch and update data in the database
+* 
+* @author FreeRoom Project Team - Julien WEBER <julien.weber@epfl.ch> and
+*         Valentin MINDER <valentin.minder@epfl.ch>
+* 
+*/
+public class PeriodicallyUpdate implements Runnable {
+
+	@Override
+	public void run() {
+		
+	}
+
+}
