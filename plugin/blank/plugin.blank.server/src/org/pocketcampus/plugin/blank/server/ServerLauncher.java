d23a630ea4e7d7faa89758681d18aaae14f3861f
Amer Chamseddine <amer@accandme.com>
2014-05-21 14:14:30 +0200

    [platform.server] can override server config using config file as argument

--- plugin/blank/plugin.blank.server/src/org/pocketcampus/plugin/blank/server/ServerLauncher.java
+++ plugin/blank/plugin.blank.server/src/org/pocketcampus/plugin/blank/server/ServerLauncher.java
@@ -5,7 +5,7 @@ import org.pocketcampus.platform.launcher.server.PocketCampusServer;
 public class ServerLauncher {
 	
 	public static void main(String[] args) throws Exception {
-		new PocketCampusServer().start();
+		new PocketCampusServer().start(args.length > 0 ? args[0] : null);
 	}
 	
 }
