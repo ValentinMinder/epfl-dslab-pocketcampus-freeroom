0d6b4bb646231b3858680b502a3817d46de05506
Loic Gardiol <loic.gardiol@gmail.com>
2014-06-05 11:00:22 +0200

    [general.ios] compiles again. Camipro widget shows fake data for now.

--- ios/PocketCampus/Plugins/CamiproPlugin/Extensions/CamiproWidget/CamiproWidgetViewController.m
+++ ios/PocketCampus/Plugins/CamiproPlugin/Extensions/CamiproWidget/CamiproWidgetViewController.m
@@ -16,13 +16,16 @@
 @property (nonatomic, strong) IBOutlet UILabel* balanceLabel;
 @property (nonatomic, strong) IBOutlet UIActivityIndicatorView* loadingIndicator;
 
-@property (nonatomic, strong) CamiproService* camiproService;
-
 @property (nonatomic, copy) void (^completionHandler)(NCUpdateResult);
 
 @end
 
 @implementation CamiproWidgetViewController
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [self widgetPerformUpdateWithCompletionHandler:NULL];
+}
 
 - (void)widgetPerformUpdateWithCompletionHandler:(void (^)(NCUpdateResult))completionHandler {
     // Perform any setup necessary in order to update the view.
@@ -31,7 +34,7 @@
     // If there's no update required, use NCUpdateResultNoData
     // If there's an update, use NCUpdateResultNewData
     
-    self.completionHandler = completionHandler;
+    /*self.completionHandler = completionHandler;
     
     if (!self.camiproService) {
         self.camiproService = [CamiproService sharedInstanceToRetain];
@@ -39,7 +42,11 @@
 
     SessionId* session = [[SessionId alloc] initWithTos:0 camiproCookie:self.camiproService.camiproSession.camiproCookie];
     CamiproRequest* request = [[CamiproRequest alloc] initWithISessionId:session iLanguage:@"fr"];
-    [self.camiproService getBalanceAndTransactions:request delegate:self];
+    [self.camiproService getBalanceAndTransactions:request delegate:self];*/
+    self.balanceLabel.text = [NSString stringWithFormat:@"CHF %.2lf", 53.00];
+    if (completionHandler) {
+        completionHandler(NCUpdateResultNewData);
+    }
 }
 
 #pragma mark - CamiproServiceDelegate
