103e339112cbf8905614b0f2623534877ca4cf74
Amer Chamseddine <amer@accandme.com>
2014-07-28 13:13:11 +0200

    [tool.global] added script that automates deployment over https

---
+++ tool/tool.global/upload_and_run.sh
@@ -0,0 +1,36 @@
+
+if [ -z "$1" ]
+then
+	echo "usage examples:"
+	echo "    ./tool/tool.global/upload_and_run.sh http://dev.pocketcampus.ch/"
+	echo "    ./tool/tool.global/upload_and_run.sh https://test-pocketcampus.epfl.ch/dev/"
+	exit
+fi
+
+server=$1
+
+echo "Access token for $server :"
+read token
+
+echo "UPLOADING JAR"
+
+uploadstatus=`curl -F file=@server/PocketCampusServer/PocketCampusServer.jar $server/upload_jar.php?t=$token`
+
+if [[ "$uploadstatus" != "Uploaded"* ]]
+then
+	echo "Upload failed:" $uploadstatus
+	exit 1
+fi
+
+echo "STARTING NEW SERVER"
+
+restartstatus=`curl -F field=value $server/restart_server.php?t=$token`
+
+if [[ "$restartstatus" != *"started with pid"* ]]
+then
+	echo "Could not start new server:" $restartstatus
+	exit 1
+fi
+
+echo "DONE"
+
