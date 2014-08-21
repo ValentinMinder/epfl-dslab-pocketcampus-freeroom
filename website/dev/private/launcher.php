0c144d2c33ac4f26cc555ab3ac5b9b6b475142a5
Amer Chamseddine <amer@accandme.com>
2014-08-22 01:30:47 +0200

    [website] prepare required files

--- website/dev/private/launcher.php
+++ website/dev/private/launcher.php
@@ -12,7 +12,12 @@ if(""  !=  exec("ps aux | grep \"php executor.php say please\" | grep -v grep") 
 
 chdir(dirname(__FILE__));
 
-system("truncate commands --size 0");
+//system("truncate commands --size 0");
+system("rm -f commands logs");
+system("touch commands logs");
+system("chown pocketcampus:apache commands logs");
+system("chmod 620 commands");
+system("chmod 640 logs");
 
 system("tail -f commands 2>/dev/null | php executor.php say please >> logs 2>&1 &");
 
