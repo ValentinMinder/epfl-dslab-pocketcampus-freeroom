dbefefe235a7a2d4b57f1446b96cf2a6d2b62581
Solal Pirelli <solal.pirelli@gmail.com>
2014-08-28 18:16:17 +0200

    [transport.wp] Forgot one file.

---
+++ Windows/Source/Transport.WindowsPhone/Views/Design/DesignSettingsViewModel.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) PocketCampus.Org 2014
+// See LICENSE file for more details
+// File author: Solal Pirelli
+
+// Design data for SettingsViewModel
+
+using PocketCampus.Transport.Services.Design;
+using PocketCampus.Transport.ViewModels;
+using ThinMvvm;
+using ThinMvvm.WindowsPhone.Design;
+
+namespace PocketCampus.Transport.Views.Design
+{
+    public sealed class DesignSettingsViewModel : DesignViewModel<SettingsViewModel, NoParameter>
+    {
+#if DEBUG
+        protected override SettingsViewModel ViewModel
+        {
+            get { return new SettingsViewModel( new DesignPluginSettings() ); }
+        }
+#endif
+    }
+}
