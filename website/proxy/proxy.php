d747e4e351c71396833872e25c5074c5f004c507
Amer Chamseddine <amer@accandme.com>
2014-08-28 20:26:44 +0200

    [website.proxy] add a facebook proxy

--- website/proxy/proxy.php
+++ website/proxy/proxy.php
@@ -25,7 +25,7 @@ $opts = array('http' =>
         'method'             => $_SERVER["REQUEST_METHOD"],
         'header'             => $headers,
         'ignore_errors'      => true,
-        'follow_location'    => 0,
+        'follow_location'    => (empty($FOLLOW_REDIRECTS) ? 0 : 1),
         'max_redirects'      => 1,
         'protocol_version'   => 1.1,
         'content'            => file_get_contents('php://input')
