293ff41f4c160d1ee1ab4ef00b7a440512499dbe
Solal Pirelli <solal.pirelli@gmail.com>
2014-08-28 23:35:11 +0200

    [satellite.wp] Refactored design data.

---
+++ Windows/Source/Satellite.WindowsPhone/Views/Design/DesignMainViewModel.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) PocketCampus.Org 2014
+// See LICENSE file for more details
+// File author: Solal Pirelli
+
+// Design data for MainViewModel
+
+using PocketCampus.Satellite.Services.Design;
+using PocketCampus.Satellite.ViewModels;
+using ThinMvvm;
+using ThinMvvm.WindowsPhone.Design;
+
+namespace PocketCampus.Satellite.Views.Design
+{
+    public sealed class DesignMainViewModel : DesignViewModel<MainViewModel, NoParameter>
+    {
+#if DEBUG
+        protected override MainViewModel ViewModel
+        {
+            get { return new MainViewModel( new DesignDataCache(), new DesignSatelliteService() ); }
+        }
+#endif
+    }
+}
