05cc5c5c54aeba5569ddbc940b3efa146a0de171
Solal Pirelli <solal.pirelli@gmail.com>
2014-08-29 00:45:52 +0200

    [news.wp] Refactored design data.

---
+++ Windows/Source/News.WindowsPhone/Views/Design/DesignFeedItemViewModel.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) PocketCampus.Org 2014
+// See LICENSE file for more details
+// File author: Solal Pirelli
+
+// Design data for FeedItemViewModel
+
+using System.Threading;
+using PocketCampus.Common.Services.Design;
+using PocketCampus.News.Models;
+using PocketCampus.News.Services.Design;
+using PocketCampus.News.ViewModels;
+using ThinMvvm.WindowsPhone.Design;
+
+namespace PocketCampus.News.Views.Design
+{
+    public sealed class DesignFeedItemViewModel : DesignViewModel<FeedItemViewModel, FeedItem>
+    {
+#if DEBUG
+        protected override FeedItemViewModel ViewModel
+        {
+            get
+            {
+                var newsService = new DesignNewsService();
+                var item = newsService.GetFeedsAsync( null, CancellationToken.None ).Result.Feeds[0].Items[0];
+                return new FeedItemViewModel( new DesignDataCache(), newsService, new DesignBrowserService(), item );
+            }
+        }
+#endif
+    }
+}
---
+++ Windows/Source/News.WindowsPhone/Views/Design/DesignMainViewModel.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) PocketCampus.Org 2014
+// See LICENSE file for more details
+// File author: Solal Pirelli
+
+// Design data for MainViewModel
+
+using PocketCampus.News.Services.Design;
+using PocketCampus.News.ViewModels;
+using ThinMvvm;
+using ThinMvvm.WindowsPhone.Design;
+
+namespace PocketCampus.News.Views.Design
+{
+    public sealed class DesignMainViewModel : DesignViewModel<MainViewModel, NoParameter>
+    {
+#if DEBUG
+        protected override MainViewModel ViewModel
+        {
+            get { return new MainViewModel( new DesignDataCache(), new DesignNewsService(), new DesignNavigationService() ); }
+        }
+#endif
+    }
+}
