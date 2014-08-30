17f69682871df8ee3e9a680006b6d538850e6ecf
Solal Pirelli <solal.pirelli@gmail.com>
2014-08-30 21:43:44 +0200

    [camipro.wp] Stop using the transaction type; as discussed by e-mail with Quentin Estoppey, it's useless now.

--- Windows/Source/Camipro/Services/Design/DesignCamiproService.cs
+++ Windows/Source/Camipro/Services/Design/DesignCamiproService.cs
@@ -37,70 +37,60 @@ namespace PocketCampus.Camipro.Services.Design
                         new Transaction
                         {
                             Amount = 120.00,
-                            Operation = "Chargement badge facture",
                             Place = "Chargement e-banking",
                             Date = new DateTime(2013, 5, 15, 11, 36, 26)
                         },
                         new Transaction
                         {
                             Amount = -2.5,
-                            Operation = "Vente",
                             Place = "COMPASS_SG_SANTAFE (334295)",
                             Date = new DateTime(2013, 5, 14, 17, 00, 15)
                         },
                         new Transaction
                         {
                             Amount = -3.0,
-                            Operation = "Vente",
                             Place = "Compass Giacometti",
                             Date = new DateTime(2013, 5, 15, 15, 13, 01)
                         },
                         new Transaction
                         {
                             Amount = -7.00,
-                            Operation = "Vente",
                             Place = "Compass Hodler 2",
                             Date = new DateTime(2013, 5, 14, 11, 41, 34)
                         },
                         new Transaction
                         {
                             Amount = -1.80,
-                            Operation = "Vente",
                             Place = "Le Négoce",
                             Date = new DateTime(2013, 5, 13, 12, 14, 26)
                         },
                         new Transaction
                         {
                             Amount = -7.00,
-                            Operation = "Vente",
                             Place = "Compass Hodler 1",
                             Date = new DateTime(2013, 5, 13, 11, 32, 14)
                         },
                         new Transaction
                         {
                             Amount = -1.90,
-                            Operation = "Vente",
                             Place = "Esplanade 2 * G. Riffault",
                             Date = new DateTime(2013, 5, 8, 11, 52, 17)
                         },
                         new Transaction
                         {
                             Amount = -7.00,
-                            Operation = "Vente",
                             Place = "Compass Hodler 2",
                             Date = new DateTime(2013, 5, 8, 11, 33, 00)
                         },
                         new Transaction
                         {
                             Amount = -1.80,
-                            Operation = "Vente",
                             Place = "SELECTA_BS_STTROPEZ (326290)",
                             Date = new DateTime(2013, 5, 8, 9, 56, 32)
                         },
                         new Transaction
                         {
                             Amount = -3.70,
-                            Operation = "Vente",
                             Place = "Le Négoce",
                             Date = new DateTime(2013, 5, 8, 12, 22, 48)
                         },
