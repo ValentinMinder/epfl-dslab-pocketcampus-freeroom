dcd97f7439c5db3b2db510dd1dd0b8abf3667487
Amer Chamseddine <amer@accandme.com>
2014-09-11 02:52:07 +0200

    [tool.merger] remove EdX from server merger

--- tool/tool.merger/merger_server.php
+++ tool/tool.merger/merger_server.php
@@ -13,7 +13,7 @@
 
 chdir(dirname(__FILE__));
 
-$plugins_to_merge = array("Authentication", "Camipro", "Moodle", "Food", "Transport", "News", "Satellite", "Map", "Directory", "PushNotif", "Events", "IsAcademia", "EdX", "CloudPrint");
+$plugins_to_merge = array("Authentication", "Camipro", "Moodle", "Food", "Transport", "News", "Satellite", "Map", "Directory", "PushNotif", "Events", "IsAcademia", "CloudPrint");
 
 $libs_to_export = array(
 		"backport-util-concurrent-3.1.jar", "bcprov-jdk15-146.jar",
