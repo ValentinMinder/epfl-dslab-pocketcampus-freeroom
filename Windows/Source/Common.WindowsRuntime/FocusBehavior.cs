8d6c2ab73bff056d74b8c2d0210a146dafacd385
Solal Pirelli <solal.pirelli@gmail.com>
2014-10-09 15:13:43 +0200

    [transport.wp81] Moved focus to a Behavior instead of code-behind.

---
+++ Windows/Source/Common.WindowsRuntime/FocusBehavior.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xaml.Interactivity;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace PocketCampus.Common
+{
+    public sealed class FocusBehavior : DependencyObject, IBehavior
+    {
+        public DependencyObject AssociatedObject { get; private set; }
+
+        public void Attach( DependencyObject associatedObject )
+        {
+            AssociatedObject = associatedObject;
+
+            var associatedControl = (Control) associatedObject;
+            associatedControl.Loaded += ( _, __ ) => associatedControl.Focus( FocusState.Pointer );
+        }
+
+        public void Detach()
+        {
+            AssociatedObject = null;
+        }
+    }
+}
--- Windows/Source/Transport.WindowsRuntime/Views/AddStationView.xaml.cs
+++ Windows/Source/Transport.WindowsRuntime/Views/AddStationView.xaml.cs
@@ -5,8 +5,6 @@
         public AddStationView()
         {
             InitializeComponent();
-            //TODO: Find a way to put this in a behavior or something (also, for some reason, Programmatic doesn't work...?)
-            Loaded += ( _, __ ) => Box.Focus( Windows.UI.Xaml.FocusState.Pointer );
         }
     }
 }
