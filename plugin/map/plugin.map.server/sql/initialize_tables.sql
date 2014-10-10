e40159165e46dccf7f91a0e0b77a34488fac5278
Loic Gardiol <loic.gardiol@gmail.com>
2014-10-10 15:05:45 +0200

    [map.server] added new layers to SQL initialization

--- plugin/map/plugin.map.server/sql/initialize_tables.sql
+++ plugin/map/plugin.map.server/sql/initialize_tables.sql
@@ -6,7 +6,11 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 /*!40101 SET NAMES utf8 */;
 
 INSERT INTO `maplayers` (`nameForQuery`, `nameForQueryAllFloors`, `name_EN`, `name_FR`) VALUES
-(1000, 'myprint-{floor}', 'myprintall', 'MyPrint printers', 'Imprimantes MyPrint'),
-(2000, 'chargeurs-{floor}', 'chargeursall', 'Camipro chargers', 'Chargeurs Camipro'),
-(3000, 'camipro-{floor}', 'camiproall', 'Camipro terminals', 'Bornes Camipro');
+(1000, 'myprint{floor}', 'myprintall', 'MyPrint printers', 'Imprimantes MyPrint'),
+(2000, 'chargeurs{floor}', 'chargeursall', 'Camipro chargers', 'Chargeurs Camipro'),
+(3000, 'bornescamipro{floor}', 'bornescamiproall', 'Camipro terminals', 'Bornes Camipro'),
+(4000, 'parkings_public{floor}', 'parkings_publicall', 'Public parking lots', 'Parkings publics'),
+(5000, 'restauration{floor}', 'restaurationall', 'Restaurants', 'Restauration'),
+(6000, 'bancomat{floor}', 'bancomatall', 'ATMs', 'Bancomats'),
+,
 
