b0b8fd655c7c0499afac2f4dd5bb2f376483e491
Solal Pirelli <solal.pirelli@gmail.com>
2014-11-23 19:23:11 +0100

    [isa.wp81] Weird hack to fix period rooms navigation.

--- Windows/Source/IsAcademia.WindowsRuntime/Views/MainView.xaml.cs
+++ Windows/Source/IsAcademia.WindowsRuntime/Views/MainView.xaml.cs
@@ -1,10 +1,24 @@
-﻿namespace PocketCampus.IsAcademia.Views
+﻿using PocketCampus.IsAcademia.ViewModels;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace PocketCampus.IsAcademia.Views
 {
     public sealed partial class MainView
     {
         public MainView()
         {
             InitializeComponent();
+        }
+
+        private void OnRoomHyperlinkTapped( object sender, TappedRoutedEventArgs e )
+        {
+            string room = (string) ( (Control) sender ).DataContext;
+            var command = ( (MainViewModel) Root.DataContext ).ViewRoomOnMapCommand;
+            if ( command.CanExecute( room ) )
+            {
+                command.Execute( room );
+            }
         }
     }
 }
