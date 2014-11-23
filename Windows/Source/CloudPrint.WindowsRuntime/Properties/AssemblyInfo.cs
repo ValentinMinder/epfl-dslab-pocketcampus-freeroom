54468fd9b34931140946057e643cbac3f80d224d
Solal Pirelli <solal.pirelli@gmail.com>
2014-11-23 22:23:56 +0100

    [cloudprint.wp81] Added skeleton.

---
+++ Windows/Source/CloudPrint.WindowsRuntime/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) PocketCampus.Org 2014
+// See LICENSE file for more details
+// File author: Solal Pirelli
+
+using System.Reflection;
+using System.Resources;
+
+[assembly: AssemblyTitle( "PocketCampus.CloudPrint.WindowsRuntime" )]
+[assembly: AssemblyCopyright( "Copyright © PocketCampus.Org 2014" )]
+[assembly: AssemblyCulture( "" )]
+[assembly: NeutralResourcesLanguageAttribute( "en" )]
+
+[assembly: AssemblyVersion( "2.4.*" )]
---
+++ Windows/Source/CloudPrint/Plugin.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) PocketCampus.Org 2014
+// See LICENSE file for more details
+// File author: Solal Pirelli
+
+using System.Collections.Generic;
+using PocketCampus.Common;
+using ThinMvvm;
+
+namespace PocketCampus.CloudPrint
+{
+    public class Plugin : IPlugin
+    {
+        public string Id
+        {
+            get { return "CloudPrint"; }
+        }
+
+        public bool IsVisible
+        {
+            get { return false; }
+        }
+
+        public bool RequiresAuthentication
+        {
+            get { return false; }
+        }
+
+        public void Initialize( INavigationService navigationService )
+        {
+        }
+
+        public void NavigateTo( INavigationService navigationService )
+        {
+
+        }
+
+        public void NavigateTo( string destination, IDictionary<string, string> parameters, INavigationService navigationService )
+        {
+
+        }
+    }
+}
