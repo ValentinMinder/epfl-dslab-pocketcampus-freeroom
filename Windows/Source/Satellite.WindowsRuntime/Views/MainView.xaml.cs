41e04d986999b5f8e141429cd3b22cfd4cc56a57
Solal Pirelli <solal.pirelli@gmail.com>
2014-12-28 15:31:39 +0100

    [satellite.wp81] Added description when tapping on beers.

--- Windows/Source/Satellite.WindowsRuntime/Views/MainView.xaml.cs
+++ Windows/Source/Satellite.WindowsRuntime/Views/MainView.xaml.cs
@@ -1,10 +1,22 @@
-﻿namespace PocketCampus.Satellite.Views
+﻿using System;
+using PocketCampus.Satellite.Models;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Input;
+
+namespace PocketCampus.Satellite.Views
 {
     public sealed partial class MainView
     {
         public MainView()
         {
             InitializeComponent();
+        }
+
+        private async void Beer_Tapped( object sender, TappedRoutedEventArgs e )
+        {
+            var beer = (Beer) ( (FrameworkElement) sender ).DataContext;
+            await new MessageDialog( beer.Description, beer.Name ).ShowAsync();
         }
     }
 }
