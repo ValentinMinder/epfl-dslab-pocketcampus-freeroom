ad35a0764269749425e4b79c40425d2e1b7da9a2
Solal Pirelli <solal.pirelli@gmail.com>
2015-02-10 16:44:34 +0100

    [tool.merger] Remove old libraries.

--- tool/tool.merger/merger_server.php
+++ tool/tool.merger/merger_server.php
@@ -18,20 +18,17 @@ $plugins_to_merge = array("Authentication", "Camipro", "Moodle", "Food", "Transp
 $libs_to_export = array( // keep this alphabetical
 		"backport-util-concurrent-3.1.jar", "bcprov-jdk15-146.jar",
 		"commons-codec-1.4.jar", "commons-io-2.0.1.jar", "commons-lang-2.6.jar", "commons-lang3-3.0.1.jar", "commons-logging-1.1.1.jar",
-		"EWSJavaAPI.jar",
 		"gcm-server.jar", "gson-1.7.1.jar", 
 		"org.json-20120521.jar",
 		"httpclient-4.1.2.jar", "httpcore-4.1.2.jar",
 		"javapns_2.2.jar",
 		"jetty-ajp-8.0.0.M3.jar", "jetty-annotations-8.0.0.M3.jar", "jetty-client-8.0.0.M3.jar", "jetty-continuation-8.0.0.M3.jar", "jetty-deploy-8.0.0.M3.jar", "jetty-http-8.0.0.M3.jar", "jetty-io-8.0.0.M3.jar", "jetty-jmx-8.0.0.M3.jar", "jetty-jndi-8.0.0.M3.jar", "jetty-overlay-deployer-8.0.0.M3.jar", "jetty-plus-8.0.0.M3.jar", "jetty-policy-8.0.0.M3.jar", "jetty-rewrite-8.0.0.M3.jar", "jetty-security-8.0.0.M3.jar", "jetty-server-8.0.0.M3.jar", "jetty-servlet-8.0.0.M3.jar", "jetty-servlets-8.0.0.M3.jar", "jetty-util-8.0.0.M3.jar", "jetty-webapp-8.0.0.M3.jar", "jetty-websocket-8.0.0.M3.jar", "jetty-xml-8.0.0.M3.jar",
 		"joda-time-2.3.jar", "json_simple-1.1.jar", "jsoup-1.7.2.jar",
-		"kxml2-2.3.0.jar",
 		"libthrift-0.9.2.jar", "log4j-1.2.16.jar",
 		"mail.jar", "mysql-connector-java-5.1.15-bin.jar",
 		"servlet-api-3.0.jar", "slf4j-api-1.6.2.jar", "slf4j-simple-1.6.2.jar",
 		"tequila-client.jar",
-		"unboundid-ldapsdk-se.jar",
-                "EWSJavaAPI.jar");
+		"unboundid-ldapsdk-se.jar");
 
 if(!empty($include_tests)) {
 	$libs_to_export[] = "junit-4.11.jar";
