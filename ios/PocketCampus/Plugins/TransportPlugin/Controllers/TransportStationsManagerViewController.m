241b9015a57b63907ffe9591e834af5aca72763d
Loic Gardiol <loic.gardiol@gmail.com>
2015-02-11 10:12:14 +0100

    [transport.ios] removed useless code

--- ios/PocketCampus/Plugins/TransportPlugin/Controllers/TransportStationsManagerViewController.m
+++ ios/PocketCampus/Plugins/TransportPlugin/Controllers/TransportStationsManagerViewController.m
@@ -76,7 +76,7 @@ static const NSUInteger kRestoreDefaultSection = 1;
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(donePressed)];
     self.navigationItem.rightBarButtonItem.style = UIBarButtonItemStylePlain;
     __weak __typeof(self) welf = self;
-    [[NSNotificationCenter defaultCenter] lga_addObserver:self name:kTransportUserTransportStationsModifiedNotification object:nil block:^(NSNotification *notif) {
+    [[NSNotificationCenter defaultCenter] lga_addObserver:self name:kTransportUserTransportStationsModifiedNotification object:self.transportService block:^(NSNotification *notif) {
         [welf refreshFromModelAnimated:YES];
     }];
     [self refreshFromModelAnimated:NO];
@@ -132,10 +132,6 @@ static const NSUInteger kRestoreDefaultSection = 1;
 #pragma mark - UITableViewDelegate
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (indexPath.section == kStationsSection) {
-        [self.tableView deselectRowAtIndexPath:indexPath animated:NO];
-        return;
-    }
     switch (indexPath.section) {
         case kStationsSection:
             [self.tableView deselectRowAtIndexPath:indexPath animated:NO];
