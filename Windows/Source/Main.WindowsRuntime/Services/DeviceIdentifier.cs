3efaeaa4fad3d365582b0575e430682e5e0ed0e1
Solal Pirelli <solal.pirelli@gmail.com>
2015-02-12 20:10:15 +0100

    [general.wp81] Fix DeviceIdentifier to return sensible values.

--- Windows/Source/Main.WindowsRuntime/Services/DeviceIdentifier.cs
+++ Windows/Source/Main.WindowsRuntime/Services/DeviceIdentifier.cs
@@ -2,8 +2,8 @@
 // See LICENSE file for more details
 // File author: Solal Pirelli
 
+using System;
 using System.Runtime.InteropServices.WindowsRuntime;
-using System.Text;
 using PocketCampus.Common.Services;
 using Windows.System.Profile;
 
@@ -20,17 +20,10 @@ namespace PocketCampus.Main.Services
                 if ( _current == null )
                 {
                     var token = HardwareIdentification.GetPackageSpecificToken( null );
-                    _current = TokenToString( token );
+                    _current = BitConverter.ToString( token.Id.ToArray() );
                 }
                 return _current;
             }
-        }
-
-
-        private static string TokenToString( HardwareToken token )
-        {
-            var bytes = token.Id.ToArray();
-            return Encoding.UTF8.GetString( bytes, 0, bytes.Length );
         }
     }
 }
