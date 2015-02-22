cbdd7cd0a0fe01b2b4f8bfeaad6a9d25bd2a9306
Solal Pirelli <solal.pirelli@gmail.com>
2015-02-22 15:14:42 +0100

    [main.wp] Fix "rate app" button in the about page.

--- windows/Source/Main.WindowsRuntime/Services/AppRatingService.cs
+++ windows/Source/Main.WindowsRuntime/Services/AppRatingService.cs
@@ -3,16 +3,19 @@
 // File author: Solal Pirelli
 
 using System;
-using Windows.ApplicationModel;
 using Windows.System;
 
 namespace PocketCampus.Main.Services
 {
     public sealed class AppRatingService : IAppRatingService
     {
+        private const string AppId = "28f8300e-8a84-4e3e-8d68-9a07c5b2a83a";
+
         public async void RequestRating()
         {
-            await Launcher.LaunchUriAsync( new Uri( "ms-windows-store:REVIEW?PFN=" + Package.Current.Id.FamilyName ) );
+            // FRAMEWORK BUG: This seems to be the only correct way to do it.
+            // Despite MSDN documentation, other ways (e.g. reviewapp without appid, or REVIEW?PFN=) open Xbox Music...
+            await Launcher.LaunchUriAsync( new Uri( "ms-windows-store:reviewapp?appid=" + AppId, UriKind.Absolute ) );
         }
     }
 }
