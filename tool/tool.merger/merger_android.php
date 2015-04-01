c3674b6fc63336cfe4e0ccdca91dd44fdecbc04a
Amer Chamseddine <amer@accandme.com>
2015-04-01 21:08:38 +0200

    [tool.merger] release android hotfix

--- tool/tool.merger/merger_android.php
+++ tool/tool.merger/merger_android.php
@@ -29,8 +29,8 @@ $path_to_plugin_dir = "../../plugin";
 $path_to_platform_dir = "../../platform";
 $path_to_lib_dir = "../../platform/platform.android/libs";
 
-$versionCode = "35";
-$versionName = "3.0";
+$versionCode = "37";
+$versionName = "3.0.1";
 
 function import_nodes($file, $tag, $doc, $parent_node, $nodes_to_remove) {
 	$doc2 = new DOMDocument();
