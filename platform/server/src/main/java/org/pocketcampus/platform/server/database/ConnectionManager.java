53e9680dc0f3b7748bd7332af0387366c4869827
Solal Pirelli <solal.pirelli@gmail.com>
2015-05-31 14:00:18 +0200

    [freeroom.server] Use separate connections.

--- platform/server/src/main/java/org/pocketcampus/platform/server/database/ConnectionManager.java
+++ platform/server/src/main/java/org/pocketcampus/platform/server/database/ConnectionManager.java
@@ -11,6 +11,9 @@ public class ConnectionManager {
 	private String user;
 	private String password;
 
+	private boolean useOwnConnection;
+	private Connection ownConnection;
+
 	public ConnectionManager(String url, String user, String password) {
 		this.url = url;
 		this.user = user;
@@ -23,16 +26,37 @@ public class ConnectionManager {
 		} catch (ClassNotFoundException e) {
 			throw new RuntimeException(e);
 		}
+
+		this.useOwnConnection=false;
 	}
 
-	private void connect() throws SQLException {
-		connection = DriverManager.getConnection(url, user, password);
+	public ConnectionManager(String url, String user, String password, boolean useOwnConnection) {
+		this.url = url;
+		this.user = user;
+		this.password = password;
+
+		// Register the JDBC driver for MySQL.
+		// TODO why do it this way? can't we just instantiate it?
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+
+		this.useOwnConnection=useOwnConnection;
 	}
 
 	public Connection getConnection() throws SQLException {
-		if (connection == null || !connection.isValid(3)) {
-			connect();
+		if(useOwnConnection){
+			if (ownConnection == null || !ownConnection.isValid(3)) {
+				ownConnection = DriverManager.getConnection(url, user, password);
+			}
+			return ownConnection;
+		}else{
+			if (connection == null || !connection.isValid(3)) {
+				connection = DriverManager.getConnection(url, user, password);
+			}
+			return connection;
 		}
-		return connection;
 	}
 }
