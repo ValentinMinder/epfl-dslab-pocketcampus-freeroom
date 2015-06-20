0507b8f0a04d547d9c9e56fb8d71dc32a1e212d8
Amer C <amer@accandme.com>
2015-06-20 20:41:33 +0200

    [website] kis servers are not even consistent

--- website/update_website.php
+++ website/update_website.php
@@ -3,7 +3,7 @@
 if (PHP_SAPI !== 'cli') {
 	die("Sorry, this can only be run from cli\n");
 }
-if (gethostname() !== "kissrv118.epfl.ch" && gethostname() !== "kissrv119.epfl.ch" && gethostname() !== "kissrv120.epfl.ch") {
+if (gethostname() !== "kissrv118.epfl.ch" && gethostname() !== "kissrv119.epfl.ch" && gethostname() !== "kissrv120") {
 	die("This script can only run on kissrv118, kissrv119, or kissrv120\n");
 }
 chdir(dirname(__FILE__) . "/../");
