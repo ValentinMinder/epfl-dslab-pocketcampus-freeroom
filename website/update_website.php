3c243d1fa90b801de0e7c4e521ba87962f634c67
Amer C <amer@accandme.com>
2015-06-20 18:28:39 +0200

    [website] make sure the update website script only runs on the correct machines

--- website/update_website.php
+++ website/update_website.php
@@ -1,10 +1,12 @@
 #!/usr/bin/php
 <?php
-if (PHP_SAPI === 'cli') {
-	chdir(dirname(__FILE__) . "/../");
-	system("git pull");
-	system("cp -r website/* /var/www/vhosts/pocketcampus/htdocs/");
-} else {
-	die("Sorry, this can only be run from cli");
+if (PHP_SAPI !== 'cli') {
+	die("Sorry, this can only be run from cli\n");
 }
+if (gethostname() !== "kissrv118" && gethostname() !== "kissrv119" && gethostname() !== "kissrv120") {
+	die("This script can only run on kissrv118, kissrv119, or kissrv120");
+}
+chdir(dirname(__FILE__) . "/../");
+system("git pull");
+system("cp -r website/* /var/www/vhosts/pocketcampus/htdocs/");
 ?>
